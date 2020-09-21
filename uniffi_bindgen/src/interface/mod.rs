65a5700b8d60578b29b1fd588befe5cc729019ae
James Hugman <james@hugman.tv>
2020-09-21 14:44:52 +0100

    Address reviewer comments

--- uniffi_bindgen/src/interface/mod.rs
+++ uniffi_bindgen/src/interface/mod.rs
@@ -609,7 +609,7 @@ impl APIConverter<Argument> for weedle::argument::SingleArgument<'_> {
         }
         let default = match self.default {
             None => None,
-            Some(v) => Some(v.value.convert(ci, &type_)?),
+            Some(v) => Some(convert_default_value(&v.value, &type_)?),
         };
         Ok(Argument {
             name: self.identifier.0.to_string(),
@@ -1062,7 +1062,7 @@ impl APIConverter<Field> for weedle::dictionary::DictionaryMember<'_> {
         }
         let default = match self.default {
             None => None,
-            Some(v) => Some(v.value.convert(ci, &type_)?),
+            Some(v) => Some(convert_default_value(&v.value, &type_)?),
         };
         Ok(Field {
             name: self.identifier.0.to_string(),
@@ -1073,21 +1073,21 @@ impl APIConverter<Field> for weedle::dictionary::DictionaryMember<'_> {
     }
 }
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize, Hash)]
-pub enum Radix {
-    Decimal = 10,
-    Octal = 8,
-    Hexadecimal = 16,
-}
-
 // Represents a literal value.
 // Used for e.g. default argument values.
 #[derive(Debug, Clone, Hash)]
 pub enum Literal {
     Boolean(bool),
     String(String),
+    // Integers are represented as the widest representation we can.
+    // Number formatting vary with language and radix, so we avoid a lot of parsing and
+    // formatting duplication by using only signed and unsigned variants.
     UInt(u64, Radix, Type),
     Int(i64, Radix, Type),
+    // Pass the string representation through as typed in the IDL.
+    // This avoids a lot of uncertainty around precision and accuracy,
+    // though bindings for languages less sophisticated number parsing than WebIDL
+    // will have to do extra work.
     Float(String, Type),
     Enum(String, Type),
     EmptySequence,
@@ -1095,86 +1095,89 @@ pub enum Literal {
     Null,
 }
 
-impl TypedAPIConverter<Literal> for weedle::literal::DefaultValue<'_> {
-    fn convert(&self, ci: &ComponentInterface, type_: &Type) -> Result<Literal> {
-        fn convert_integer(
-            literal: &weedle::literal::IntegerLit<'_>,
-            type_: &Type,
-        ) -> Result<Literal> {
-            use regex::Regex;
+// Represent the radix of integer literal values.
+// We preserve the radix into the generated bindings for readability reasons.
+#[derive(Debug, Clone, Copy, Hash)]
+pub enum Radix {
+    Decimal = 10,
+    Octal = 8,
+    Hexadecimal = 16,
+}
 
-            let (string, radix) = match literal {
-                weedle::literal::IntegerLit::Dec(v) => (v.0, Radix::Decimal),
-                weedle::literal::IntegerLit::Hex(v) => (v.0, Radix::Hexadecimal),
-                weedle::literal::IntegerLit::Oct(v) => (v.0, Radix::Octal),
-            };
-            // This is the radix of the parsed number, passed to `from_str_radix`.
-            let src_radix = radix as u32;
-            // This radix tells the backends how to represent the number in the output languages.
-            let dest_radix = if string == "0" || string.starts_with('-') {
-                // 1. weedle parses "0" as an octal literal, but we most likely want to treat this as a decimal.
-                // 2. Explicitly negatively signed hex numbers won't convert via i64 very well if they're not 64 bit.
-                //    For ease of implementation, output will use decimal.
-                Radix::Decimal
-            } else {
-                radix
-            };
+fn convert_default_value(
+    default_value: &weedle::literal::DefaultValue<'_>,
+    type_: &Type,
+) -> Result<Literal> {
+    fn convert_integer(literal: &weedle::literal::IntegerLit<'_>, type_: &Type) -> Result<Literal> {
+        use regex::Regex;
 
-            let trimmer = Regex::new("^(-?)(0x)?").unwrap();
-            let string = trimmer.replace_all(string, "$1").to_lowercase();
+        let (string, radix) = match literal {
+            weedle::literal::IntegerLit::Dec(v) => (v.0, Radix::Decimal),
+            weedle::literal::IntegerLit::Hex(v) => (v.0, Radix::Hexadecimal),
+            weedle::literal::IntegerLit::Oct(v) => (v.0, Radix::Octal),
+        };
+        // This is the radix of the parsed number, passed to `from_str_radix`.
+        let src_radix = radix as u32;
+        // This radix tells the backends how to represent the number in the output languages.
+        let dest_radix = if string == "0" || string.starts_with('-') {
+            // 1. weedle parses "0" as an octal literal, but we most likely want to treat this as a decimal.
+            // 2. Explicitly negatively signed hex numbers won't convert via i64 very well if they're not 64 bit.
+            //    For ease of implementation, output will use decimal.
+            Radix::Decimal
+        } else {
+            radix
+        };
 
-            Ok(match type_ {
-                Type::Int8 | Type::Int16 | Type::Int32 | Type::Int64 => Literal::Int(
-                    i64::from_str_radix(&string, src_radix)?,
-                    dest_radix,
-                    type_.clone(),
-                ),
-                Type::UInt8 | Type::UInt16 | Type::UInt32 | Type::UInt64 => Literal::UInt(
-                    u64::from_str_radix(&string, src_radix)?,
-                    dest_radix,
-                    type_.clone(),
-                ),
+        let trimmer = Regex::new("^(-?)(0x)?").unwrap();
+        let string = trimmer.replace_all(string, "$1").to_lowercase();
 
-                _ => bail!("Cannot coerce literal {} into a non-integer type", string),
-            })
-        }
+        Ok(match type_ {
+            Type::Int8 | Type::Int16 | Type::Int32 | Type::Int64 => Literal::Int(
+                i64::from_str_radix(&string, src_radix)?,
+                dest_radix,
+                type_.clone(),
+            ),
+            Type::UInt8 | Type::UInt16 | Type::UInt32 | Type::UInt64 => Literal::UInt(
+                u64::from_str_radix(&string, src_radix)?,
+                dest_radix,
+                type_.clone(),
+            ),
 
-        fn convert_float(literal: &weedle::literal::FloatLit<'_>, type_: &Type) -> Result<Literal> {
-            let string = match literal {
-                weedle::literal::FloatLit::Value(v) => v.0,
-
-                _ => bail!("Infinity and NaN is not currently supported"),
-            };
-
-            Ok(match type_ {
-                Type::Float32 | Type::Float64 => {
-                    Literal::Float(string.to_string(), type_.clone())
-                }
-                _ => bail!("Cannot coerce literal {} into a non-float type", string),
-            })
-        }
-
-        Ok(match (self, type_) {
-            (weedle::literal::DefaultValue::Boolean(b), Type::Boolean) => Literal::Boolean(b.0),
-            (weedle::literal::DefaultValue::String(s), Type::String) => {
-                Literal::String(s.0.to_string())
-            }
-            (weedle::literal::DefaultValue::EmptyArray(_), Type::Sequence(_)) => {
-                Literal::EmptySequence
-            }
-            (weedle::literal::DefaultValue::String(s), Type::Enum(_)) => {
-                Literal::Enum(s.0.to_string(), type_.clone())
-            }
-            (weedle::literal::DefaultValue::Null(_), Type::Optional(_)) => Literal::Null,
-            (_, Type::Optional(inner)) => self.convert(ci, inner)?,
-
-            // We'll ensure the type safety in the convert_* number methods.
-            (weedle::literal::DefaultValue::Integer(i), _) => convert_integer(i, type_)?,
-            (weedle::literal::DefaultValue::Float(i), _) => convert_float(i, type_)?,
-
-            _ => bail!("No support for {:?} literal yet", self),
+            _ => bail!("Cannot coerce literal {} into a non-integer type", string),
         })
     }
+
+    fn convert_float(literal: &weedle::literal::FloatLit<'_>, type_: &Type) -> Result<Literal> {
+        let string = match literal {
+            weedle::literal::FloatLit::Value(v) => v.0,
+
+            _ => bail!("Infinity and NaN is not currently supported"),
+        };
+
+        Ok(match type_ {
+            Type::Float32 | Type::Float64 => Literal::Float(string.to_string(), type_.clone()),
+            _ => bail!("Cannot coerce literal {} into a non-float type", string),
+        })
+    }
+
+    Ok(match (default_value, type_) {
+        (weedle::literal::DefaultValue::Boolean(b), Type::Boolean) => Literal::Boolean(b.0),
+        (weedle::literal::DefaultValue::String(s), Type::String) => {
+            Literal::String(s.0.to_string())
+        }
+        (weedle::literal::DefaultValue::EmptyArray(_), Type::Sequence(_)) => Literal::EmptySequence,
+        (weedle::literal::DefaultValue::String(s), Type::Enum(_)) => {
+            Literal::Enum(s.0.to_string(), type_.clone())
+        }
+        (weedle::literal::DefaultValue::Null(_), Type::Optional(_)) => Literal::Null,
+        (_, Type::Optional(inner_type)) => convert_default_value(default_value, &inner_type)?,
+
+        // We'll ensure the type safety in the convert_* number methods.
+        (weedle::literal::DefaultValue::Integer(i), _) => convert_integer(&i, type_)?,
+        (weedle::literal::DefaultValue::Float(i), _) => convert_float(&i, type_)?,
+
+        _ => bail!("No support for {:?} literal yet", default_value),
+    })
 }
 
 /// Represents an attribute parsed from WebIDL, like [ByRef] or [Throws].
