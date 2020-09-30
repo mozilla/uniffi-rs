fd9a7384ca01926fa15e6e5cc47274730f5e1ba6
James Hugman <james@hugman.tv>
2020-09-30 20:20:26 +0100

    Add build phase to run uniffi_bindgen generate from Xcode

--- examples/app/ios/IOSApp-Bridging-Header.h
+++ examples/app/ios/IOSApp-Bridging-Header.h
@@ -1,12 +1,11 @@
-//
-//  IOSApp-Bridging-Header.h
-//  IOSApp
-//
-//  Created by James Hugman on 30/09/2020.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 
 #ifndef IOSApp_Bridging_Header_h
 #define IOSApp_Bridging_Header_h
 
+#import <TodoList.h>
 
 #endif /* IOSApp_Bridging_Header_h */
