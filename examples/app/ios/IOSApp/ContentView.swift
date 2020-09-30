62c01eea3e93bb6573131225900c854016983a5f
James Hugman <james@hugman.tv>
2020-09-30 20:21:02 +0100

    SwiftUI is the best UI

--- examples/app/ios/IOSApp/ContentView.swift
+++ examples/app/ios/IOSApp/ContentView.swift
@@ -8,12 +8,24 @@ struct ContentView: View {
     @State private var clicked = 0
     var body: some View {
         VStack {
-            if clicked > 0 {
+            if clicked <= 0 {
+                Text("Click me!").padding()
+            } else {
                 Text("Clicked \(clicked) times!").padding()
             }
-            Button("Click me") {
-                clicked = clicked + 1
-            }.padding()
+
+            HStack {
+                if clicked > 0 {
+                    Button("-") {
+                        clicked = clicked - 1
+                    }.padding()
+                } else {
+                    Text("-").padding()
+                }
+                Button("+") {
+                    clicked = clicked + 1
+                }.padding()
+            }
         }
     }
 }
