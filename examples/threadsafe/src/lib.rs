f143ac179fc170ff33f217b0455c66d2bf47afce
James Hugman <james@hugman.tv>
2021-01-15 15:35:28 +0000

    Integration example to working from multiple kotlin threads

---
+++ examples/threadsafe/src/lib.rs
@@ -0,0 +1,83 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use std::sync::atomic::{AtomicBool, AtomicI32, Ordering};
+
+/// Simulation of a task doing something to keep a thread busy.
+/// Up until now, everything has been synchronous and blocking, so
+/// we can use this to simulate a CPU bound or I/O task.
+fn busy_wait(cycles: i32) -> i32 {
+    let mut res = 1;
+    for _ in 0..cycles {
+        // Dumb as rocks divergent function that the compiler _probably_
+        // can't optimize away.
+        res = res * -1;
+    }
+    res
+}
+
+struct Counter {
+    is_busy: bool,
+    count: i32,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Self {
+            is_busy: false,
+            count: 0,
+        }
+    }
+
+    fn busy_wait(&mut self, cycles: i32) -> i32 {
+        self.is_busy = true;
+        let res = busy_wait(cycles);
+        self.is_busy = false;
+        res
+    }
+
+    fn increment_if_busy(&mut self) -> i32 {
+        if self.is_busy {
+            self.count = self.count + 1;
+        }
+
+        self.count
+    }
+}
+
+struct ThreadsafeCounter {
+    is_busy: AtomicBool,
+    count: AtomicI32,
+}
+
+unsafe impl Send for ThreadsafeCounter {}
+unsafe impl Sync for ThreadsafeCounter {}
+
+impl ThreadsafeCounter {
+    fn new() -> Self {
+        Self {
+            is_busy: AtomicBool::new(false),
+            count: AtomicI32::new(0),
+        }
+    }
+
+    fn busy_wait(&self, cycles: i32) -> i32 {
+        self.is_busy.store(true, Ordering::SeqCst);
+        let res = busy_wait(cycles);
+        self.is_busy.store(false, Ordering::SeqCst);
+        res
+    }
+
+    fn increment_if_busy(&self) -> i32 {
+        let current = if self.is_busy.load(Ordering::SeqCst) {
+            self.count.fetch_add(1, Ordering::SeqCst) + 1
+        } else {
+            self.count.load(Ordering::SeqCst)
+        };
+
+        current
+    }
+}
+
+include!(concat!(env!("OUT_DIR"), "/threadsafe.uniffi.rs"));
---
+++ examples/threadsafe/tests/bindings/test_threadsafe.kts
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import java.util.concurrent.*
+import uniffi.threadsafe.*
+
+/// We'd like to write objects so different methods be executed on multiple 
+/// threads at the same time.
+///
+/// We have two Rust implementations of the same object.
+/// The objects can be busy-wait, thus blocking the caller thread for sometime.
+/// On a different thread, the same object can increment a counter. The counter
+/// should only increment when when busy.
+///
+/// This function calls the busy-wait on one thread, and the incrementIfBusy 
+/// multiple times on another.
+fun countConcurrently(busyWait: () -> Int, incrementIfBusy: () -> Int): Int {
+    val executor = Executors.newFixedThreadPool(3)
+    return try {
+        val busyWaiting: Future<Int> = executor.submit(Callable {
+            busyWait()
+        })
+
+        val incrementing: Future<Int> = executor.submit(Callable {
+            var count = 0
+            for (n in 1..100) {
+                count = incrementIfBusy()
+            }
+            count
+        })
+
+        busyWaiting.get()
+        incrementing.get()
+    } finally {
+        executor.shutdown()
+    }
+}
+
+// busyWait will cycle for this many iterations.
+// The faster your machine, the higher this needs to simulate busy waiting
+// long enough for us to incrementIfBusy.
+// If the Rust compiler gets too smart, and optimizes the busy wait loop away,
+// then no number high enough will make the tests work.
+val WAIT_FOR = 100_000
+
+/// The first implementation uses Uniffi's default locking strategy.
+/// Unfortunately, this counter does not work as we would like: uniffi
+/// holds a mutex throughout the busyWait, and incrementIfBusy is only
+/// called once that mutex is cleared, when it's not busy anymore.
+Counter().use { counter -> 
+    val count = countConcurrently(
+        { counter.busyWait(WAIT_FOR) },
+        { counter.incrementIfBusy() }
+    )
+    assert(count == 0) { "Uniffi doing the locking: incrementIfBusy=$count" }
+}
+
+/// The second implementation is marked `[Threadsafe]` in the UDL,
+/// which declares that the Rust library code will look after locking itself,
+/// and not rely on the default locking strategy.
+ThreadsafeCounter().use { counter -> 
+    val count = countConcurrently(
+        { counter.busyWait(WAIT_FOR) },
+        { counter.incrementIfBusy() }
+    )
+    assert(count > 0) { "Counter doing the locking: incrementIfBusy=$count" }
+}
