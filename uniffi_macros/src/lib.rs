ef40e522816b95247431d5158951493429f5e79f
Ryan Kelly <ryan@rfk.id.au>
2021-02-12 11:48:22 +1100

    A little experiment with macros instead of build scripts.
    This commit is basically me messing around with procedural macros,
    to try them out as a potential future iteration of the UniFFI
    developer experience.
    
    As a crate author, I think it would be amazing not to have to
    worry about .udl files and build scripts and what-not, but
    instead be able to declare my API with a macro and have Rust
    just take care of the details. This commit is a tiny experiment
    in that direction, based on existing features of UniFFI.
    
    The idea is that I can wrap my Rust code in a little macro
    like this:
    
    ```
    [uniffi_macros::declare_interface]
    pub mod my_component {
      ...my rust code goes here...
    }
    ```
    
    And I can provide a corresponding `my_component.udl` file that
    declares the interface, and all the Rust scaffolding code just
    gets taken care of automagically.
    
    I still have to provide a `.udl` file, but I hope you can imagine
    one day the possibility of generating the interface definition
    directly from the Rust code that is decorated by the macro.
    Not any time soon! But one day...

--- uniffi_macros/src/lib.rs
+++ uniffi_macros/src/lib.rs
@@ -9,6 +9,9 @@
 
 use quote::{format_ident, quote};
 use std::env;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::PathBuf;
 use syn::{bracketed, punctuated::Punctuated, LitStr, Token};
 
@@ -83,4 +86,138 @@ impl syn::parse::Parse for FilePaths {
             test_scripts,
         })
     }
+}
+
+/// A helper macro to include generated component scaffolding.
+///
+/// This is a simple convenience macro to include the UniFFI component
+/// scaffolding as built by `uniffi_build::generate_scaffolding`.
+/// Use it like so:
+///
+/// ```rs
+/// uniffi_macros::include_scaffolding!("my_component_name");
+/// ```
+///
+/// This will expand to the appropriate `include!` invocation to include
+/// the generated `my_component_name.uniffi.rs` (which it assumes has
+/// been successfully built by your crate's `build.rs` script).
+//
+#[proc_macro]
+pub fn include_scaffolding(component_name: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let name = syn::parse_macro_input!(component_name as syn::LitStr);
+    if std::env::var("OUT_DIR").is_err() {
+        quote! {
+            compile_error!("This macro assumes the crate has a build.rs script, but $OUT_DIR is not present");
+        }
+    } else {
+        quote! {
+            include!(concat!(env!("OUT_DIR"), "/", #name, ".uniffi.rs"));
+        }
+    }.into()
+}
+
+/// A (very naughty) macro to generate and include UniFFI component scaffolding.
+///
+/// This is a (very naughty) macro that can be used by UniFFI component crates to
+/// automatically generate the UniFFI component scaffolding and include it into their
+/// Rust code. It's intended as a simpler alternative to a build script that runs
+/// `uniffi-bindgen`, and can be used like this:
+///
+/// ```rs
+///
+/// #[uniffi_macros::declare_interface]
+/// mod my_example {
+///     // The Rust code to implement the component goes here.
+///     pub struct Example {
+///         pub fn hello(&self) -> String {
+///             String::new("world")
+///         }
+///     }
+/// }
+/// ```
+///
+/// This macro call would read a UniFFI component interface definition from `my_example.udl`,
+/// generate the corresponding Rust scaffolding code, combine it with the Rust code from the
+/// inline module, and make the result available as top-level items in the containing file.
+///
+/// What's so naughty about this, you ask? Well, the ideal macro is supposed to be a pure
+/// function of its input tokens to its output tokens, but this one does some very
+/// frowned-upon things behind the scenes:
+///
+///  * it accesses a separate file in order to find the component interface definition.
+///  * it runs `uniffi-bindgen` and writes its output to disk.
+///  * it parses the resulting file back into memory to include in its return value.
+///
+/// All in all, some pretty weird behaviours for a macro. However, you can imagine it
+/// evolving to do fewer of these things in the future.
+///
+/// A future version could do the scaffolding generation in-process rather than
+/// shelling out to `uniffi-bindgen`. A future version could directly generate a Rust
+/// tokenstream rather than re-parsing the scaffolding from a string. And a far, far
+/// future version could conceivably even parse the attributed Rust code in order to
+/// determine the interface of the component, rather than depending on a separate `.udl`
+/// file at all...
+///
+/// For now, consider this an experiment to see whether we like the macro syntax
+/// from a developer-experience perspective.
+///
+#[proc_macro_attribute]
+pub fn declare_interface(
+    _attrs: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    // We expect to be applied to an inline module declaration, whose name
+    // is the name of the `.udl` file to load.
+    let module = syn::parse_macro_input!(item as syn::ItemMod);
+    let component_name = module.ident.to_string();
+    // Locate file relative to `src` directory of current crate.
+    let file_name = format!("{}.udl", component_name);
+    let pkg_dir = env::var("CARGO_MANIFEST_DIR")
+        .expect("Missing $CARGO_MANIFEST_DIR, cannot generate scaffolding for current crate");
+    let file_path: PathBuf = [&pkg_dir, "src", &file_name].iter().collect();
+    // Shell out to the uniffi-bindgen build process.
+    // Note that `uniffi_bindgen` expects to be writing into $OUT_DIR, but we can't assume that exists
+    // (it only seems to be present when the crate has a build script) and so need to create a tempdir.
+    let out_dir = tempfile::tempdir()
+        .expect("Could not generate temporary directly in which to run uniffi-bindgen");
+    run_uniffi_bindgen_scaffolding(out_dir.path().as_os_str(), file_path.as_os_str());
+    // Slurp the resulting code back in, so we can include it in our output.
+    let mut scaffolding = String::new();
+    let mut f = File::open(out_dir.path().join(format!("{}.uniffi.rs", component_name)))
+        .expect("Failed to open uniffi-bindgen output file");
+    f.read_to_string(&mut scaffolding)
+        .expect("Failed to read uniffi-bindgen output file");
+    let scaffolding: syn::File =
+        syn::parse_str(scaffolding.as_str()).expect("Failed to parse uniffi-bindgen output file");
+    // Lift the contents of the inline module up to the top level.
+    let mut module_items = match module.content {
+        None => vec![],
+        Some((_, items)) => items,
+    };
+    // Include the generated scaffolding.
+    module_items.extend(scaffolding.items);
+    // And we're done!
+    proc_macro::TokenStream::from(quote! {
+        #(#module_items)*
+    })
+}
+
+#[cfg(not(feature = "builtin-bindgen"))]
+fn run_uniffi_bindgen_scaffolding(out_dir: &OsStr, udl_file: &OsStr) -> () {
+    let status = std::process::Command::new("uniffi-bindgen")
+        .arg("scaffolding")
+        .arg("--out-dir")
+        .arg(out_dir)
+        .arg(udl_file)
+        .status()
+        .expect("failed to run `uniffi-bindgen` - have you installed it via `cargo install uniffi_bindgen`?");
+    if !status.success() {
+        panic!("Eror while generating scaffolding code with uniffi-bindgen");
+    }
+}
+
+#[cfg(feature = "builtin-bindgen")]
+fn run_uniffi_bindgen_scaffolding(out_dir: &OsStr, udl_file: &OsStr) -> () {
+    uniffi_bindgen::generate_component_scaffolding(udl_file, None, Some(out_dir), None, false)
+        .expect("Failed to generate scaffolding code");
 }
