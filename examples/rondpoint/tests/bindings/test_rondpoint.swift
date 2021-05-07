61db09af1635aee5a3a2327f17cda90cb8474bdc
James Hugman <james@hugman.tv>
2021-05-07 11:12:04 +0100

    Debug swift bindings

--- examples/rondpoint/tests/bindings/test_rondpoint.swift
+++ examples/rondpoint/tests/bindings/test_rondpoint.swift
@@ -1,3 +1,4 @@
+import Foundation
 import rondpoint
 
 let dico = Dictionnaire(un: .deux, deux: false, petitNombre: 0, grosNombre: 123456789)
@@ -58,6 +59,17 @@ let rt = Retourneur()
 ["", "abc", "null\0byte", "été", "ښي لاس ته لوستلو لوستل", "😻emoji 👨‍👧‍👦multi-emoji, 🇨🇭a flag, a canal, panama"]
     .affirmAllerRetour(rt.identiqueString)
 
+[["a": [1,2,3]], ["a": true]]
+    .affirmAllerRetour(rt.identiqueJsonValue) { a, b in toString(a) == toString(b) }
+
+func toString(_ jsonObject: [String: Any]) -> String {
+    if let data = try? JSONSerialization.data(withJSONObject: jsonObject, options: []),
+        let string = String(data: data, encoding: .utf8) {
+        return string
+    }
+    return "{}"
+}
+
 // Test one way across the FFI.
 //
 // We send one representation of a value to lib.rs, and it transforms it into another, a string.
@@ -103,6 +115,8 @@ assert("uniffi 💚 swift!" == wellKnown, "wellKnownString 'uniffi 💚 swift!' 
 // Doubles
 [.zero, 1, -1, .leastNonzeroMagnitude, .greatestFiniteMagnitude].affirmEnchaine(st.toStringDouble) { Double.init($0) == $1 }
 
+[["a": [1,2,3]], ["a": true]].affirmEnchaine(st.toStringJsonValue) { $0 == toString($1) }
+
 // Some extension functions for testing the results of roundtripping and stringifying
 extension Array where Element: Equatable {
     static func defaultEquals(_ observed: String, expected: Element) -> Bool {
@@ -119,7 +133,24 @@ extension Array where Element: Equatable {
 
     func affirmAllerRetour(_ fn: (Element) -> Element) {
         self.forEach { v in
-            assert(fn(v) == v, "identique_\(type(of:v))(\(v))")
+            let obs = fn(v)
+            assert(obs == v, "identique_\(type(of:v)): observed=\(obs), expected=\(v)")
+        }
+    }
+}
+
+extension Array {
+    func affirmAllerRetour(_ fn: (Element) -> Element, equals: (Element, Element) -> Bool) {
+        self.forEach { v in
+            let obs = fn(v)
+            assert(equals(obs, v), "identique_\(type(of:v)): observed=\(obs), expected=\(v)")
+        }
+    }
+
+    func affirmEnchaine(_ fn: (Element) -> String, equals: (String, Element) -> Bool) {
+        self.forEach { v in
+            let obs = fn(v)
+            assert(equals(obs, v), "toString_\(type(of:v))(\(v)): observed=\(obs), expected=\(v)")
         }
     }
 }
--- uniffi_bindgen/src/bindings/swift/templates/RustBufferHelper.swift
+++ uniffi_bindgen/src/bindings/swift/templates/RustBufferHelper.swift
@@ -205,7 +205,7 @@ extension String: ViaFfi {
 }
 
 fileprivate enum JSONValue {
-    fileprivate typealias JSONObject = Dictionary<String, Any>
+    fileprivate typealias JSONObject = [String: Any]
     fileprivate typealias FfiType = RustBuffer
     
     fileprivate static func lift(_ v: FfiType) throws -> JSONObject {
@@ -227,17 +227,17 @@ fileprivate enum JSONValue {
     }
 
     fileprivate static func lower(_ jsonObject: JSONObject) -> FfiType {
-        let jsonString = JSONValue.intoString(jsonObject)
+        let jsonString = JSONValue.stringify(jsonObject)
         return jsonString.lower()
     }
 
     fileprivate static func write(_ jsonObject: JSONObject, into buf: Writer) {
-        let jsonString = JSONValue.intoString(jsonObject)
+        let jsonString = JSONValue.stringify(jsonObject)
         return jsonString.write(into: buf)
     }
 
-    fileprivate static func intoString(_ jsonObject: JSONObject) -> String {
-        if let data = try? JSONSerialization.data(withJSONObject: self, options: []),
+    fileprivate static func stringify(_ jsonObject: JSONObject) -> String {
+        if let data = try? JSONSerialization.data(withJSONObject: jsonObject, options: []),
             let string = String(data: data, encoding: .utf8) {
             return string
         }
