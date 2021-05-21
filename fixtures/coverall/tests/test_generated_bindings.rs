2ffa1bdf08d90e0a7e6b40c0925d30ce770cf45d
Ryan Kelly <ryan@rfk.id.au>
2021-05-21 15:29:41 +1000

    Fix Kotlin codegen for writing optional integers.
    Seems like you can't use `v=== null` on when `v` is an optional primitive
    type like `UByte?`.

--- fixtures/coverall/tests/test_generated_bindings.rs
+++ fixtures/coverall/tests/test_generated_bindings.rs
@@ -1,4 +1,7 @@
 uniffi_macros::build_foreign_language_testcases!(
     "src/coverall.udl",
-    ["tests/bindings/test_coverall.py"]
+    [
+        "tests/bindings/test_coverall.py",
+        "tests/bindings/test_coverall.kts"
+    ]
 );
