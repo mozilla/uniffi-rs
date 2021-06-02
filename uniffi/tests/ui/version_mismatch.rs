b9c1e5c7d1869b5c72aec7fe9d48b53010a67197
Mark Hammond <mhammond@skippinet.com.au>
2021-06-02 14:30:51 +1000

    giving up in disgust

--- uniffi/tests/ui/version_mismatch.rs
+++ uniffi/tests/ui/version_mismatch.rs
@@ -1,6 +1,4 @@
 // This should fail with a version mismatch.
-// *sob* - someone fixed the grammar on this output!
-// normalize-stderr-test: ", which would overflow" -> "which would overflow"
 uniffi::assert_compatible_version!("0.0.1"); // An error message would go here.
 
 fn main() {}
