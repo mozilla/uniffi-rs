0fbce14d790dc9f0c1361f3ec7dc910d6dcf8f97
Jan-Erik Rediger <janerik@fnordig.de>
2021-08-03 13:04:28 +0200

    Document JNA minimum version requirement.

--- uniffi_bindgen/src/bindings/swift/templates/BridgingHeaderTemplate.h
+++ uniffi_bindgen/src/bindings/swift/templates/BridgingHeaderTemplate.h
@@ -11,15 +11,15 @@
 // We ensure they are declared exactly once, with a header guard, UNIFFI_SHARED_H.
 #ifdef UNIFFI_SHARED_H
     // We also try to prevent mixing versions of shared uniffi header structs.
-    // If you add anything to the #else block, you must increment the version suffix in UNIFFI_SHARED_HEADER_V2
-    #ifndef UNIFFI_SHARED_HEADER_V2
+    // If you add anything to the #else block, you must increment the version suffix in UNIFFI_SHARED_HEADER_V3
+    #ifndef UNIFFI_SHARED_HEADER_V3
         #error Combining helper code from multiple versions of uniffi is not supported
-    #endif // ndef UNIFFI_SHARED_HEADER_V2
+    #endif // ndef UNIFFI_SHARED_HEADER_V3
 #else
 #define UNIFFI_SHARED_H
-#define UNIFFI_SHARED_HEADER_V2
+#define UNIFFI_SHARED_HEADER_V3
 // ⚠️ Attention: If you change this #else block (ending in `#endif // def UNIFFI_SHARED_H`) you *must* ⚠️
-// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V2 in this file.           ⚠️
+// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V3 in this file.           ⚠️
 
 typedef struct RustBuffer
 {
@@ -41,7 +41,7 @@ typedef struct RustCallStatus {
 } RustCallStatus;
 
 // ⚠️ Attention: If you change this #else block (ending in `#endif // def UNIFFI_SHARED_H`) you *must* ⚠️
-// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V2 in this file.           ⚠️
+// ⚠️ increment the version suffix in all instances of UNIFFI_SHARED_HEADER_V3 in this file.           ⚠️
 #endif // def UNIFFI_SHARED_H
 
 {% for func in ci.iter_ffi_function_definitions() -%}
