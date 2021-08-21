cd42e1ec2279930241a31435ce526d5497d9214b
Alex Rahalevich <alex.rahalevich@dr-bill.ca>
2021-08-20 22:30:14 -0700

    add timestamp support for ruby

--- fixtures/uniffi-fixture-time/tests/test_generated_bindings.rs
+++ fixtures/uniffi-fixture-time/tests/test_generated_bindings.rs
@@ -4,5 +4,6 @@ uniffi_macros::build_foreign_language_testcases!(
         "tests/bindings/test_chronological.py",
         "tests/bindings/test_chronological.kts",
         "tests/bindings/test_chronological.swift",
+        "tests/bindings/test_chronological.rb",
     ]
 );
