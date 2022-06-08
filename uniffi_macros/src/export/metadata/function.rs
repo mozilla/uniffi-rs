f36068e2af01eac32eb2bb77024b431ffec3b12a
Jonas Platte <jplatte@matrix.org>
2022-06-08 18:38:31 +0200

    Support impl blocks in the proc-macro frontend

--- uniffi_macros/src/export/metadata/function.rs
+++ uniffi_macros/src/export/metadata/function.rs
@@ -2,49 +2,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use uniffi_meta::{FnMetadata, FnParamMetadata};
+use uniffi_meta::FnMetadata;
 
-use super::convert_type;
+use super::convert::{fn_param_metadata, return_type_metadata};
 use crate::export::ExportItem;
 
 pub(super) fn gen_fn_metadata(sig: syn::Signature, mod_path: &[String]) -> syn::Result<ExportItem> {
     let metadata = fn_metadata(&sig, mod_path)?;
 
-    Ok(ExportItem::Function { sig, metadata })
-}
-
-fn fn_metadata(sig: &syn::Signature, mod_path: &[String]) -> syn::Result<FnMetadata> {
-    let return_type = match &sig.output {
-        syn::ReturnType::Default => None,
-        syn::ReturnType::Type(_, ty) => Some(convert_type(ty)?),
-    };
-
-    Ok(FnMetadata {
-        module_path: mod_path.to_owned(),
-        name: sig.ident.to_string(),
-        inputs: sig
-            .inputs
-            .iter()
-            .map(|a| fn_param_metadata(a, false))
-            .collect::<syn::Result<_>>()?,
-        return_type,
+    Ok(ExportItem::Function {
+        sig: Box::new(sig),
+        metadata,
     })
 }
 
-fn fn_param_metadata(a: &syn::FnArg, _is_method: bool) -> syn::Result<FnParamMetadata> {
-    let (name, ty) = match a {
-        syn::FnArg::Receiver(_) => unimplemented!(),
-        syn::FnArg::Typed(pat_ty) => {
-            let name = match &*pat_ty.pat {
-                syn::Pat::Ident(pat_id) => pat_id.ident.to_string(),
-                _ => unimplemented!(),
-            };
-            (name, &pat_ty.ty)
-        }
-    };
-
-    Ok(FnParamMetadata {
-        name,
-        ty: convert_type(ty)?,
+fn fn_metadata(sig: &syn::Signature, mod_path: &[String]) -> syn::Result<FnMetadata> {
+    Ok(FnMetadata {
+        module_path: mod_path.to_owned(),
+        name: sig.ident.to_string(),
+        inputs: fn_param_metadata(&sig.inputs)?,
+        return_type: return_type_metadata(&sig.output)?,
     })
 }
