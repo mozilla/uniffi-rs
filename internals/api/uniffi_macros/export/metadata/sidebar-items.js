5ec5beb5a58550468297e9c6eb17d3ff9e253944
skhamis <skhamis@users.noreply.github.com>
2022-06-23 21:13:16 +0000

    Built artifacts of 5d2c6be9 [ci skip]

--- internals/api/uniffi_macros/export/metadata/sidebar-items.js
+++ internals/api/uniffi_macros/export/metadata/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["convert_type",""],["gen_metadata",""],["write_json_metadata",""]],"mod":[["function",""]],"static":[["METADATA_DIR",""]]});
+initSidebarItems({"fn":[["arg_as_type",""],["convert_bare_type_name",""],["convert_generic_type",""],["convert_generic_type1",""],["convert_generic_type2",""],["convert_type",""],["gen_metadata",""],["type_as_type_path",""],["type_not_supported",""],["write_json_metadata",""]],"mod":[["function",""]],"static":[["METADATA_DIR",""]]});
