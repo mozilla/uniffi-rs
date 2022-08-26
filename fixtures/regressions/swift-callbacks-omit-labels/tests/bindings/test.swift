00bf5082cbf9dee702c0798ec59f51c60ab83789
Jan-Erik Rediger <jrediger@mozilla.com>
2022-08-26 14:57:44 +0200

    Apply `omit_argument_labels` option when calling into a callback
    This also adds a test to make sure we're not breaking it again.
    
    Fixes #1312

---
+++ fixtures/regressions/swift-callbacks-omit-labels/tests/bindings/test.swift
@@ -0,0 +1,26 @@
+// Regression test for https://github.com/mozilla/uniffi-rs/issues/1312
+//
+// Really this can be detected at compile time:
+// We didn't apply the same `omit_argument_labels` configuration to callbacks,
+// leading to a compilation error.
+// To make sure everything gets called right though we write a full test here.
+
+import regression_test_callbacks_omit_labels
+
+class ClientDelegateImpl: ClientDelegate {
+    var recvCount: Int = 0
+    var lastFriend = ""
+
+    func didReceiveFriendRequest(_ user: UserModel) {
+        recvCount += 1
+        lastFriend = user.name
+    }
+}
+
+let cd = ClientDelegateImpl()
+let client = Client()
+
+client.friendRequest(cd)
+
+assert(cd.recvCount == 1, "delegate should be called once")
+assert(cd.lastFriend == "Alice", "delegate should have received Alice's friend request")
