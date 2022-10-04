9b729d11f8241f9b462d22fbc33e3f338f71bd1f
Jonas Platte <jplatte@matrix.org>
2022-10-04 15:26:18 +0200

    Set a more useful span for type assertions in proc-macro output

--- uniffi_macros/src/object.rs
+++ uniffi_macros/src/object.rs
@@ -1,5 +1,5 @@
 use proc_macro2::{Ident, Span, TokenStream};
-use quote::quote;
+use quote::{quote, quote_spanned};
 use syn::DeriveInput;
 use uniffi_meta::ObjectMetadata;
 
@@ -11,6 +11,9 @@ pub fn expand_object(input: DeriveInput, module_path: Vec<String>) -> TokenStrea
     let metadata = ObjectMetadata { module_path, name };
     let free_fn_ident = Ident::new(&metadata.free_ffi_symbol_name(), Span::call_site());
     let meta_static_var = create_metadata_static_var(ident, metadata.into());
+    let type_assertion = quote_spanned! {ident.span()=>
+        ::uniffi::deps::static_assertions::assert_type_eq_all!(#ident, crate::uniffi_types::#ident);
+    };
 
     quote! {
         #[doc(hidden)]
@@ -28,8 +31,7 @@ pub fn expand_object(input: DeriveInput, module_path: Vec<String>) -> TokenStrea
             });
         }
 
-        ::uniffi::deps::static_assertions::assert_type_eq_all!(#ident, crate::uniffi_types::#ident);
-
         #meta_static_var
+        #type_assertion
     }
 }
