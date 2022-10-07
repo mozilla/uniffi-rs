e0ee322186b0ce0fac9af729a725ec3b080810e5
bendk <bendk@users.noreply.github.com>
2022-10-07 14:01:36 +0000

    Built artifacts of f6787147 [ci skip]

--- internals/api/implementors/syn/parse/trait.Parse.js
+++ internals/api/implementors/syn/parse/trait.Parse.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["uniffi_macros"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/syn/1.0.101/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"uniffi_macros/struct.FilePaths.html\" title=\"struct uniffi_macros::FilePaths\">FilePaths</a>","synthetic":false,"types":["uniffi_macros::FilePaths"]}];
+implementors["uniffi_macros"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/syn/1.0.102/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"uniffi_macros/struct.FilePaths.html\" title=\"struct uniffi_macros::FilePaths\">FilePaths</a>","synthetic":false,"types":["uniffi_macros::FilePaths"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
