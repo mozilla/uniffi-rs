6348fce21e40cf57aa0445b8376d01b5a62363b2
bendk <bendk@users.noreply.github.com>
2022-10-07 14:02:49 +0000

    Built artifacts of c00230a1 [ci skip]

--- internals/api/uniffi_macros/util/sidebar-items.js
+++ internals/api/uniffi_macros/util/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["create_metadata_static_var",""],["mod_path",""],["rewrite_self_type","Rewrite Self type alias usage in an impl block to the type itself."]]};
+window.SIDEBAR_ITEMS = {"fn":[["assert_type_eq",""],["create_metadata_static_var",""],["mod_path",""],["rewrite_self_type","Rewrite Self type alias usage in an impl block to the type itself."]]};
