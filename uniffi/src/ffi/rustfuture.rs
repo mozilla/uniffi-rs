147820ffb5485f12de83cb4aebb8ef075ea531aa
Ivan Enderlin <ivan@mnt.io>
2022-11-22 15:09:15 +0100

    doc: Write missing documentation.

--- uniffi/src/ffi/rustfuture.rs
+++ uniffi/src/ffi/rustfuture.rs
@@ -10,7 +10,19 @@ use std::{
     task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
 };
 
-/// `RustFuture` must have the size of a pointer.
+/// `RustFuture` represents a `Future` that can be sent over FFI safely-ish.
+///
+/// The `RustFuture` is parameterized by `T` which implements `FfiConverter`.
+/// Thus, the `Future` outputs of value of kind `FfiConverter::RustType`.
+/// The `poll` method maps this `FfiConverter::RustType` to
+/// `FfiConverter::FfiType` when the inner `Future` is ready.
+///
+/// This type is not instantiated directly, but via the procedural macros,
+/// e.g. `#[uniffi::export]`. A `RustFuture` is created, boxed, and then is
+/// manipulated by “assistant” functions, resp. `uniffi_rustfuture_poll` and
+/// `uniffi_rustfuture_drop`. Because the `RustFuture` type contains a generic
+/// parameter `T`, the procedural macros will do a monomorphisation phase so that
+/// all the API has all their types known.
 #[repr(transparent)]
 pub struct RustFuture<T>(
     Pin<Box<dyn Future<Output = <T as FfiConverter>::RustType> + Send + 'static>>,
@@ -22,6 +34,8 @@ impl<T> RustFuture<T>
 where
     T: FfiConverter,
 {
+    /// Create a new `RustFuture` from a regular `Future` that outputs a value
+    /// of kind `FfiConverter::RustType`.
     pub fn new<F>(future: F) -> Self
     where
         F: Future<Output = <T as FfiConverter>::RustType> + Send + 'static,
@@ -29,13 +43,22 @@ where
         Self(Box::pin(future))
     }
 
+    /// Poll the future. It basically maps
+    /// `<T as FfiConverter>::RustType` to `<T as FfiConverter>::FfiType` when
+    /// the inner future is ready.
+    ///
+    /// There is one subtlety compared to `Future::poll` though: the
+    /// `foreign_waker_pointer`. We want the `RustFuture` **to be driven by
+    /// the foreign language's runtime/executor**. Hence the possibility for
+    /// the foreign language to provide its own waker function: Rust can signal
+    /// something has happened within the future and should be polled again.
     pub fn poll(
         &mut self,
-        waker_pointer: *const RustFutureForeignWaker,
+        foreign_waker_pointer: *const RustFutureForeignWaker,
     ) -> Poll<<T as FfiConverter>::FfiType> {
         let waker = unsafe {
             Waker::from_raw(RawWaker::new(
-                Arc::into_raw(Arc::new(waker_pointer)) as *const (),
+                Arc::into_raw(Arc::new(foreign_waker_pointer)) as *const (),
                 &RustTaskWakerBuilder::VTABLE,
             ))
         };
@@ -65,21 +88,34 @@ where
     }
 }
 
+impl<T> FfiDefault for Poll<T> {
+    /// The default value for `Poll<T>` is `Poll::Pending`.
+    fn ffi_default() -> Self {
+        Self::Pending
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_rust_future_size() {
-        assert_eq!(
-            mem::size_of_val(&RustFuture::<bool>::new(async { unreachable!() })),
-            mem::size_of::<*const u8>(),
-        );
+        let pointer_size = mem::size_of::<*const u8>();
+        let rust_future_size = pointer_size * 2;
+
+        assert_eq!(mem::size_of::<RustFuture::<bool>>(), rust_future_size,);
+        assert_eq!(mem::size_of::<RustFuture::<u64>>(), rust_future_size,);
     }
 }
 
+/// Type alias to a function with a C ABI. It defines the shape of
+/// the foreign language's waker which is called by the `RustFuture` (more
+/// precisely, by its inner future) to signal the foreign language that
+/// something has happened. See [`RustFuture::poll`] to learn more.
 pub type RustFutureForeignWaker = extern "C" fn();
 
+/// Zero-sized type to create the VTable for the `RawWaker`.
 struct RustTaskWakerBuilder;
 
 impl RustTaskWakerBuilder {
@@ -112,12 +148,6 @@ impl RustTaskWakerBuilder {
     unsafe fn drop_waker(foreign_waker: *const ()) {
         let waker: *const RustFutureForeignWaker = mem::transmute(foreign_waker);
         drop(Arc::from_raw(waker));
-    }
-}
-
-impl<T> FfiDefault for Poll<T> {
-    fn ffi_default() -> Self {
-        Self::Pending
     }
 }
 
