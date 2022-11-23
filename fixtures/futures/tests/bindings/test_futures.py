0111cf3c0d8c859cd686fddd38cbd873d09eb21a
Ivan Enderlin <ivan@mnt.io>
2022-11-23 09:30:22 +0100

    test: Add tests for Python.

---
+++ fixtures/futures/tests/bindings/test_futures.py
@@ -0,0 +1,56 @@
+from uniffi_futures import sleep, say_after
+import unittest
+from datetime import datetime
+import asyncio
+
+def now():
+    return datetime.now()
+
+class TestFutures(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual('foo', 'foo')
+
+    def test_sleep(self):
+        async def test():
+            t0 = now()
+            await sleep(2)
+            t1 = now()
+
+            t_delta = (t1 - t0).total_seconds()
+            self.assertTrue(t_delta > 2 and t_delta < 2.1)
+
+        asyncio.run(test())
+
+    def test_sequential_futures(self):
+        async def test():
+            t0 = now()
+            result_alice = await say_after(1, 'Alice')
+            result_bob = await say_after(2, 'Bob')
+            t1 = now()
+
+            t_delta = (t1 - t0).total_seconds()
+            self.assertTrue(t_delta > 3 and t_delta < 3.1)
+            self.assertEqual(result_alice, 'Hello, Alice!')
+            self.assertEqual(result_bob, 'Hello, Bob!')
+
+        asyncio.run(test())
+
+    def test_concurrent_tasks(self):
+        async def test():
+            alice = asyncio.create_task(say_after(1, 'Alice'))
+            bob = asyncio.create_task(say_after(2, 'Bob'))
+
+            t0 = now()
+            result_alice = await alice
+            result_bob = await bob
+            t1 = now()
+
+            t_delta = (t1 - t0).total_seconds()
+            self.assertTrue(t_delta > 2 and t_delta < 2.1)
+            self.assertEqual(result_alice, 'Hello, Alice!')
+            self.assertEqual(result_bob, 'Hello, Bob!')
+
+        asyncio.run(test())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ fixtures/futures/tests/test_generated_bindings.rs
@@ -0,0 +1,4 @@
+uniffi_macros::build_foreign_language_testcases!(
+    ["src/uniffi_futures.udl",],
+    ["tests/bindings/test_futures.py",]
+);
