c6175abab1253991840a8dfd329bc605eed0f4ae
Ivan Enderlin <ivan@mnt.io>
2022-12-16 14:56:16 +0100

    doc: Add a better demo.

--- fixtures/futures/examples/kotlin/demo.kt
+++ fixtures/futures/examples/kotlin/demo.kt
@@ -2,29 +2,27 @@ package my.demo
 
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
+import kotlin.system.*
 import uniffi.fixture.futures.*
 
 fun main() = runBlocking {
-    launch {
-        println("greet(\"Gordon\"): ${greet("Gordon")}")
-        println("alwaysReady: ${alwaysReady()}")
-        println("say: ${say()}")
-        println("sayAfter: ${sayAfter(2.toUByte(), "Alice")}")
-        println("bbb")
+    val time1 = measureTimeMillis {
+        val alice = sayAfter(2.toUByte(), "Alice")
+        val bob = sayAfter(3.toUByte(), "Bob")
+
+        println("alice: ${alice}")
+        println("bob: ${bob}")
     }
-    println("aaa")
+
+    println("time: ${time1}")
+
+    val time2 = measureTimeMillis {
+        val alice = async { sayAfter(2.toUByte(), "Alice") }
+        val bob = async { sayAfter(2.toUByte(), "Bob") }
+
+        println("alice: ${alice.await()}")
+        println("bob: ${bob.await()}")
+    }
+
+    println("time: ${time2}")
 }
-
-suspend fun foo(): Boolean = suspendCoroutine<Boolean> { continuation ->
-    // do something
-    continuation.resume(true)
-}
-
-
-
-/*
-
-
-// or suspendCancellableCoroutine<bool> { continuation -> … }
-
-*/
