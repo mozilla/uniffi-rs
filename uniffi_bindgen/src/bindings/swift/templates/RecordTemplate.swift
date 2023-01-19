c2e55100b2b512bd2f66ea154bf7c3f19389f427
Mark Hammond <mhammond@skippinet.com.au>
2023-01-20 09:12:46 +1100

    Add nits from #1442

--- uniffi_bindgen/src/bindings/swift/templates/RecordTemplate.swift
+++ uniffi_bindgen/src/bindings/swift/templates/RecordTemplate.swift
@@ -50,7 +50,7 @@ public struct {{ ffi_converter_name }}: FfiConverterRustBuffer {
 }
 
 {#
-We always write these public functions just incase the struct is used as
+We always write these public functions just in case the struct is used as
 an external type by another crate.
 #}
 public func {{ ffi_converter_name }}_lift(_ buf: RustBuffer) throws -> {{ type_name }} {
