4f464fd8ab30e7addad6ae21b486e292eac93eea
Luca Campobasso <lcampobasso@gmail.com>
2023-01-24 16:08:35 +0100

    fix

--- uniffi_bindgen/src/bindings/swift/templates/TopLevelFuncDocsTemplate.swift
+++ uniffi_bindgen/src/bindings/swift/templates/TopLevelFuncDocsTemplate.swift
@@ -2,7 +2,7 @@
 /// fun name: {{func.name()}}
 /// for help: https://developer.apple.com/documentation/docc
 /// - Parameters:
-{ % for arg in func. arguments() -% }
+{% for arg in func.arguments() -% }
 ///   - {{ arg.name() }}: argument description
+{% endfor % }
 /// - Returns: The sloth's energy level after eating.
-{ % endfor % }
