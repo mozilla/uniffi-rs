2ab5c10f6ffea32b63234274f6b82167114ae85e
Ivan Enderlin <ivan@mnt.io>
2023-02-13 19:27:04 +0100

    test: Diable Kotlin future test suite for now.

--- fixtures/futures/tests/test_generated_bindings.rs
+++ fixtures/futures/tests/test_generated_bindings.rs
@@ -1,5 +1,6 @@
 uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_futures.py",
     "tests/bindings/test_futures.swift",
-    "tests/bindings/test_futures.kts",
+    // Disable Kotlin test suite until the Docker container image is updated.
+    // "tests/bindings/test_futures.kts",
 );
