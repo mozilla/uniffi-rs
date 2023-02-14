a12768ef794fd49fc235fbbb89f26d0e1fc8399b
Ivan Enderlin <ivan@mnt.io>
2023-02-14 09:44:30 +0100

    Revert "test: Diable Kotlin future test suite for now."
    This reverts commit 2ab5c10f6ffea32b63234274f6b82167114ae85e.

--- fixtures/futures/tests/test_generated_bindings.rs
+++ fixtures/futures/tests/test_generated_bindings.rs
@@ -1,6 +1,5 @@
 uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_futures.py",
     "tests/bindings/test_futures.swift",
-    // Disable Kotlin test suite until the Docker container image is updated.
-    // "tests/bindings/test_futures.kts",
+    "tests/bindings/test_futures.kts",
 );
