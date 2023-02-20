8d30f0bad37ec8c73d0d0637ec0632231136fd08
Ivan Enderlin <ivan@mnt.io>
2023-02-20 15:20:24 +0100

    test: Disable Kotlin tests for `fixtures/futures/`.

--- fixtures/futures/tests/test_generated_bindings.rs
+++ fixtures/futures/tests/test_generated_bindings.rs
@@ -1,5 +1,7 @@
 uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_futures.py",
     "tests/bindings/test_futures.swift",
-    "tests/bindings/test_futures.kts",
+    // Disable due to the Docker image being outdated for now
+    // Please see https://github.com/mozilla/uniffi-rs/pull/1409#issuecomment-1433871031.
+    // "tests/bindings/test_futures.kts",
 );
