8872f408269aa316fade8b6cf603850e9fc05c56
sagudev <16504129+sagudev@users.noreply.github.com>
2023-02-25 09:55:11 +0100

    Use AttributedType in DictionaryMembers

--- weedle2/src/dictionary.rs
+++ weedle2/src/dictionary.rs
@@ -1,16 +1,16 @@
 use crate::attribute::ExtendedAttributeList;
 use crate::common::{Default, Identifier};
-use crate::types::Type;
+use crate::types::AttributedType;
 
 /// Parses dictionary members
 pub type DictionaryMembers<'a> = Vec<DictionaryMember<'a>>;
 
 ast_types! {
-    /// Parses dictionary member `[attributes]? required? type identifier ( = default )?;`
+    /// Parses dictionary member `[attributes]? required? attributedtype identifier ( = default )?;`
     struct DictionaryMember<'a> {
         attributes: Option<ExtendedAttributeList<'a>>,
         required: Option<term!(required)>,
-        type_: Type<'a>,
+        type_: AttributedType<'a>,
         identifier: Identifier<'a>,
         default: Option<Default<'a>>,
         semi_colon: term!(;),
@@ -29,5 +29,15 @@ mod test {
         required.is_some();
         identifier.0 == "num";
         default.is_some();
+    });
+
+    test!(should_parse_dictionary_member_with_attributed_type { "required [EnforceRange] unsigned long num = 5;" =>
+        "";
+        DictionaryMember;
+        attributes.is_none();
+        required.is_some();
+        identifier.0 == "num";
+        default.is_some();
+        type_.attributes.is_some();
     });
 }
