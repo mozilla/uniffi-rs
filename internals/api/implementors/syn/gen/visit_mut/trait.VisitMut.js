f8ffcab452e3b13e5b15bbfe1fe096a4abc9fac3
mhammond <mhammond@users.noreply.github.com>
2023-07-24 14:08:33 +0000

    Built artifacts of 826ea995 [ci skip]

--- internals/api/implementors/syn/gen/visit_mut/trait.VisitMut.js
+++ internals/api/implementors/syn/gen/visit_mut/trait.VisitMut.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"uniffi_macros":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/syn/2.0.26/syn/gen/visit_mut/trait.VisitMut.html\" title=\"trait syn::gen::visit_mut::VisitMut\">VisitMut</a> for <a class=\"struct\" href=\"uniffi_macros/export/struct.RewriteSelfVisitor.html\" title=\"struct uniffi_macros::export::RewriteSelfVisitor\">RewriteSelfVisitor</a>&lt;'a&gt;"]]
+"uniffi_macros":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/syn/2.0.27/syn/gen/visit_mut/trait.VisitMut.html\" title=\"trait syn::gen::visit_mut::VisitMut\">VisitMut</a> for <a class=\"struct\" href=\"uniffi_macros/export/struct.RewriteSelfVisitor.html\" title=\"struct uniffi_macros::export::RewriteSelfVisitor\">RewriteSelfVisitor</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
