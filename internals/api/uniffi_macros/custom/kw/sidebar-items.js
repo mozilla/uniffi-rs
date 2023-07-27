2e77b37a1850af804a145172942cd59c883cedd7
mhammond <mhammond@users.noreply.github.com>
2023-07-27 14:50:25 +0000

    Built artifacts of 93634167 [ci skip]

---
+++ internals/api/uniffi_macros/custom/kw/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["builtin","newtype","tag"]};
---
+++ internals/api/uniffi_macros/custom/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["custom_ffi_converter_impl","custom_ffi_type_converter","expand_custom","expand_ffi_converter_custom_type"],"mod":["kw"],"struct":["CustomTypeAttr"]};
