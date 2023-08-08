884a9aa0ce5fce7d356f26377a1e74fdef24b6a4
mhammond <mhammond@users.noreply.github.com>
2023-08-08 18:26:45 +0000

    Built artifacts of 81fd29d8 [ci skip]

--- internals/api/uniffi_macros/custom/kw/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["builtin","newtype","tag"]};
--- internals/api/uniffi_macros/custom/sidebar-items.js
+++ internals/api/uniffi_macros/custom/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["custom_ffi_converter_impl","custom_ffi_type_converter","expand_custom","expand_ffi_converter_custom_type"],"mod":["kw"],"struct":["CustomTypeAttr"]};
+window.SIDEBAR_ITEMS = {"fn":["custom_ffi_type_converter","expand_ffi_converter_custom_newtype","expand_ffi_converter_custom_type"]};
