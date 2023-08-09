mozilla/uniffi-rs

commit 93634167f234eda2e891c45f0c7a9b9310fe73dc

Allow procmacros to define custom types (#1661)

uniffi_macros/src/custom.rs
@@ -1,0 +1,185 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use crate::util::{
+    either_attribute_arg, ident_to_string, mod_path, parse_comma_separated, tagged_impl_header,
+    AttributeSliceExt, UniffiAttributeArgs,
+};
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream},
+    Data, DeriveInput, Path, Token,
+};
+
+pub fn expand_custom(input: DeriveInput) -> syn::Result<TokenStream> {
+    if !matches!(input.data, Data::Struct(_)) {
+        return Err(syn::Error::new(
+            Span::call_site(),
+            "This derive must only be used on structs",
+        ));
+    };
+
+    let ident = &input.ident;
+    let attr = input.attrs.parse_uniffi_attr_args::<CustomTypeAttr>()?;
+
+    let ffi_converter = custom_ffi_converter_impl(ident, &attr)?;
+
+    let type_converter = match attr.newtype {
+        Some(builtin) => custom_ffi_type_converter(ident, &builtin)?,
+        None => TokenStream::new(),
+    };
+    Ok(quote! {
+        #ffi_converter
+        #type_converter
+    })
+}
+
+pub(crate) fn expand_ffi_converter_custom_type(
+    attr: CustomTypeAttr,
+    input: DeriveInput,
+) -> syn::Result<TokenStream> {
+    custom_ffi_converter_impl(&input.ident, &attr)
+}
+
+// For custom scaffolding types we need to generate an FfiConverter impl based on the
+// UniffiCustomTypeConverter implementation that the library supplies
+pub(crate) fn custom_ffi_converter_impl(
+    ident: &Ident,
+    attr: &CustomTypeAttr,
+) -> syn::Result<TokenStream> {
+    if attr.builtin.is_some() && attr.newtype.is_some() {
+        return Err(syn::Error::new(
+            Span::call_site(),
+            "Custom types must not specify both `builtin` and `newtype`",
+        ));
+    }
+    let Some(builtin) = attr.builtin.as_ref().or(attr.newtype.as_ref()) else {
+        return Err(syn::Error::new(
+            Span::call_site(),
+            "Custom types must specify the builtin/newtype",
+        ));
+    };
+    let ffi_name = match builtin.to_string().as_str() {
+        "i8" | "u8" | "i16" | "u16" | "i32" | "u32" | "i64" | "u64" | "f32" | "f64" => {
+            builtin.to_string()
+        }
+        "String" => "::uniffi::RustBuffer".to_string(),
+        // in theory we could support others, but for now...
+        _ => {
+            return Err(syn::Error::new(
+                Span::call_site(),
+                "Type is not supported as a custom type builtin",
+            ));
+        }
+    };
+
+    let ffi_path: TokenStream = ffi_name.parse()?;
+
+    let impl_spec = tagged_impl_header("FfiConverter", ident, attr.tag.as_ref());
+    let name = ident_to_string(ident);
+    let mod_path = mod_path()?;
+
+    Ok(quote! {
+        #[automatically_derived]
+        unsafe #impl_spec {
+            type FfiType = #ffi_path;
+            fn lower(obj: #ident ) -> Self::FfiType {
+                <#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::lower(<#ident as UniffiCustomTypeConverter>::from_custom(obj))
+            }
+
+            fn try_lift(v: Self::FfiType) -> uniffi::Result<#ident> {
+                <#ident as UniffiCustomTypeConverter>::into_custom(<#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::try_lift(v)?)
+            }
+
+            fn write(obj: #ident, buf: &mut Vec<u8>) {
+                <#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::write(<#ident as UniffiCustomTypeConverter>::from_custom(obj), buf);
+            }
+
+            fn try_read(buf: &mut &[u8]) -> uniffi::Result<#ident> {
+                <#ident as UniffiCustomTypeConverter>::into_custom(<#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::try_read(buf)?)
+            }
+
+            ::uniffi::ffi_converter_default_return!(crate::UniFfiTag);
+
+            const TYPE_ID_META: ::uniffi::MetadataBuffer = ::uniffi::MetadataBuffer::from_code(::uniffi::metadata::codes::TYPE_CUSTOM)
+                .concat_str(#mod_path)
+                .concat_str(#name)
+                .concat(<#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::TYPE_ID_META);
+        }
+    })
+}
+
+fn custom_ffi_type_converter(ident: &Ident, builtin: &Ident) -> syn::Result<TokenStream> {
+    Ok(quote! {
+        impl UniffiCustomTypeConverter for #ident {
+            type Builtin = #builtin;
+
+            fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+                Ok(#ident(val))
+            }
+
+            fn from_custom(obj: Self) -> Self::Builtin {
+                obj.0
+            }
+        }
+    })
+}
+
+mod kw {
+    syn::custom_keyword!(tag);
+    syn::custom_keyword!(builtin);
+    syn::custom_keyword!(newtype);
+}
+
+#[derive(Default)]
+pub(crate) struct CustomTypeAttr {
+    tag: Option<Path>,
+    builtin: Option<Ident>,
+    newtype: Option<Ident>,
+}
+
+impl UniffiAttributeArgs for CustomTypeAttr {
+    fn parse_one(input: ParseStream<'_>) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(kw::tag) {
+            let _: kw::tag = input.parse()?;
+            let _: Token![=] = input.parse()?;
+            Ok(Self {
+                tag: Some(input.parse()?),
+                ..Self::default()
+            })
+        } else if lookahead.peek(kw::builtin) {
+            let _: kw::builtin = input.parse()?;
+            let _: Token![=] = input.parse()?;
+            Ok(Self {
+                builtin: Some(input.parse()?),
+                ..Self::default()
+            })
+        } else if lookahead.peek(kw::newtype) {
+            let _: kw::newtype = input.parse()?;
+            let _: Token![=] = input.parse()?;
+            Ok(Self {
+                newtype: Some(input.parse()?),
+                ..Self::default()
+            })
+        } else {
+            Err(lookahead.error())
+        }
+    }
+
+    fn merge(self, other: Self) -> syn::Result<Self> {
+        Ok(Self {
+            tag: either_attribute_arg(self.tag, other.tag)?,
+            builtin: either_attribute_arg(self.builtin, other.builtin)?,
+            newtype: either_attribute_arg(self.newtype, other.newtype)?,
+        })
+    }
+}
+
+impl Parse for CustomTypeAttr {
+    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
+        parse_comma_separated(input)
+    }
+}

uniffi_macros/src/lib.rs
@@ -17,6 +17,7 @@ use syn::{
     parse_macro_input, Ident, LitStr, Token,
 };
 
+mod custom;
 mod enum_;
 mod error;
 mod export;
@@ -28,8 +29,8 @@ mod test;
 mod util;
 
 use self::{
-    enum_::expand_enum, error::expand_error, export::expand_export, object::expand_object,
-    record::expand_record,
+    custom::expand_custom, enum_::expand_enum, error::expand_error, export::expand_export,
+    object::expand_object, record::expand_record,
 };
 
 /// A macro to build testcases for a component's generated bindings.
@@ -114,6 +115,28 @@ pub fn derive_error(input: TokenStream) -> TokenStream {
     expand_error(parse_macro_input!(input))
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
+}
+
+#[proc_macro_derive(CustomType, attributes(uniffi))]
+pub fn derive_custom_type(input: TokenStream) -> TokenStream {
+    expand_custom(parse_macro_input!(input))
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+
+/// Generate the FfiConverter implementation for an Custom Type
+///
+/// This is used by the Askama scaffolding code.  It this inputs an struct/enum definition, but
+/// only outputs the `FfiConverter` implementation, not the item.
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn ffi_converter_custom_type(attrs: TokenStream, input: TokenStream) -> TokenStream {
+    custom::expand_ffi_converter_custom_type(
+        syn::parse_macro_input!(attrs),
+        syn::parse_macro_input!(input),
+    )
+    .unwrap_or_else(syn::Error::into_compile_error)
+    .into()
 }
 
 /// Generate the FfiConverter implementation for a Record

fixtures/ext-types/proc-macro-lib/src/lib.rs
@@ -136,6 +136,54 @@ fn get_uniffi_one_interface() -> Arc<UniffiOneInterface> {
     Arc::new(UniffiOneInterface::new())
 }
 
+// Some custom types via macros.
+// Another guid - here we use a regular struct.
+#[derive(uniffi::CustomType)]
+#[uniffi(builtin=String)]
+pub struct Uuid {
+    val: String,
+}
+
+impl UniffiCustomTypeConverter for Uuid {
+    type Builtin = String;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Ok(Uuid { val })
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.val
+    }
+}
+
+// A custom type using the "newtype" idiom.
+// Uniffi can generate the UniffiCustomTypeConverter for us.
+#[derive(uniffi::CustomType)]
+#[uniffi(newtype=i64)]
+pub struct NewtypeHandle(i64);
+
+#[uniffi::export]
+fn get_uuid(u: Option<Uuid>) -> Uuid {
+    u.unwrap_or_else(|| Uuid {
+        val: "new".to_string(),
+    })
+}
+
+#[uniffi::export]
+fn get_uuid_value(u: Uuid) -> String {
+    u.val
+}
+
+#[uniffi::export]
+fn get_newtype_handle(u: Option<NewtypeHandle>) -> NewtypeHandle {
+    u.unwrap_or(NewtypeHandle(42))
+}
+
+#[uniffi::export]
+fn get_newtype_handle_value(u: NewtypeHandle) -> i64 {
+    u.0
+}
+
 #[uniffi::export]
 fn get_guid_procmacro(g: Option<Guid>) -> Guid {
     ext_types_guid::get_guid(g)

fixtures/ext-types/proc-macro-lib/tests/bindings/test_imported_types.py
@@ -4,6 +4,7 @@
 
 import unittest
 import urllib
+from ext_types_guid import *
 from imported_types_lib import *
 from uniffi_one import *
 
@@ -50,6 +51,18 @@ class TestIt(unittest.TestCase):
     def test_get_guid_procmacro(self):
         g = get_guid_procmacro(None)
         self.assertEqual(g, get_guid_procmacro(g))
+
+    def test_get_customs(self):
+        g = get_guid(None)
+        self.assertEqual(g, get_guid(g))
+
+        u = get_uuid(None)
+        self.assertEqual(u, get_uuid(u))
+        self.assertEqual(get_uuid_value(u), "new")
+
+        h = get_newtype_handle(None)
+        self.assertEqual(h, get_newtype_handle(h))
+        self.assertEqual(get_newtype_handle_value(h), 42)
 
 if __name__=='__main__':
     unittest.main()

uniffi_bindgen/src/scaffolding/templates/ExternalTypesTemplate.rs
@@ -14,44 +14,9 @@
 // For custom scaffolding types we need to generate an FfiConverter impl based on the
 // UniffiCustomTypeConverter implementation that the library supplies
 {% for (name, builtin) in ci.iter_custom_types() %}
-{% if loop.first %}
-
-// A trait that's in our crate for our external wrapped types to implement.
-trait UniffiCustomTypeConverter {
-    type Builtin;
-
-    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> where Self: Sized;
-    fn from_custom(obj: Self) -> Self::Builtin;
-}
-
-{%- endif -%}
 
 // Type `{{ name }}` wraps a `{{ builtin|debug }}`
+#[::uniffi::ffi_converter_custom_type(builtin = {{ builtin|type_rs }}, tag = crate::UniFfiTag)]
+struct r#{{ name }} { }
 
-unsafe impl uniffi::FfiConverter<crate::UniFfiTag> for r#{{ name }} {
-    type FfiType = {{ FfiType::from(builtin).borrow()|type_ffi }};
-
-    fn lower(obj: {{ name }} ) -> Self::FfiType {
-        {{ builtin|ffi_converter }}::lower(<{{ name }} as UniffiCustomTypeConverter>::from_custom(obj))
-    }
-
-    fn try_lift(v: Self::FfiType) -> uniffi::Result<{{ name }}> {
-        <r#{{ name }} as UniffiCustomTypeConverter>::into_custom({{ builtin|ffi_converter }}::try_lift(v)?)
-    }
-
-    fn write(obj: {{ name }}, buf: &mut Vec<u8>) {
-        {{ builtin|ffi_converter }}::write(<{{ name }} as UniffiCustomTypeConverter>::from_custom(obj), buf);
-    }
-
-    fn try_read(buf: &mut &[u8]) -> uniffi::Result<r#{{ name }}> {
-        <{{ name }} as UniffiCustomTypeConverter>::into_custom({{ builtin|ffi_converter }}::try_read(buf)?)
-    }
-
-    ::uniffi::ffi_converter_default_return!(crate::UniFfiTag);
-
-    const TYPE_ID_META: ::uniffi::MetadataBuffer = ::uniffi::MetadataBuffer::from_code(::uniffi::metadata::codes::TYPE_CUSTOM)
-        .concat_str("{{ ci.namespace() }}")
-        .concat_str("{{ name }}")
-        .concat({{ builtin|ffi_converter }}::TYPE_ID_META);
-}
 {%- endfor -%}

uniffi_macros/src/setup_scaffolding.rs
@@ -113,5 +113,12 @@ pub fn setup_scaffolding(namespace: String) -> Result<TokenStream> {
             };
         }
 
+        // A trait that's in our crate for our external wrapped types to implement.
+        #[allow(unused)]
+        trait UniffiCustomTypeConverter {
+            type Builtin;
+            fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> where Self: Sized;
+            fn from_custom(obj: Self) -> Self::Builtin;
+        }
     })
 }

commit fc8a722c36bba9b8c136a6613460e41b5a87c95b

Re-allow more complex custom types in UDL (#1690)
* Add http-headermap custom type fixture

`http::HeaderMap` is a multi-map so for simplicity we expose as a `sequence` of
headers.

This was broken by https://github.com/mozilla/uniffi-rs/pull/1661 which caused:

```
error: unexpected token
   --> /tmp/target/debug/build/uniffi-fixture-ext-types-http-headermap-2750e41fdae052ad/out/http_headermap.uniffi.rs:132:40
    |
132 | ::uniffi::custom_type!(r#HeaderMap, std::vec::Vec<r#HttpHeader>);
    |                                        ^
```

* Use builtin's `FfiConverter::FfiType` when implementing custom type `FfiConverter`

This avoids needing to match on stringified Rust types.

This is the first part of the fix for the `http-headmap` fixture.

* Allow custom type's builtin to be a `Path` not just an `Ident`

This allows the `http-headermap` test to pass again.

Cargo.toml
@@ -26,6 +26,7 @@ members = [
   "fixtures/callbacks",
 
   "fixtures/ext-types/guid",
+  "fixtures/ext-types/http-headermap",
   "fixtures/ext-types/uniffi-one",
   "fixtures/ext-types/lib",
   "fixtures/ext-types/proc-macro-lib",

fixtures/ext-types/http-headermap/Cargo.toml
@@ -1,0 +1,24 @@
+[package]
+name = "uniffi-fixture-ext-types-http-headermap"
+edition = "2021"
+version = "0.22.0"
+license = "MPL-2.0"
+publish = false
+
+[lib]
+crate-type = ["lib", "cdylib"]
+name = "ext_types_http_headermap"
+
+[dependencies]
+anyhow = "1"
+bytes = "1.3"
+http = "0.2.9"
+serde_json = "1"
+thiserror = "1.0"
+uniffi = {path = "../../../uniffi", version = "0.24" }
+
+[build-dependencies]
+uniffi = {path = "../../../uniffi", version = "0.24", features = ["build"] }
+
+[dev-dependencies]
+uniffi = {path = "../../../uniffi", version = "0.24", features = ["bindgen-tests"] }

fixtures/ext-types/http-headermap/build.rs
@@ -1,0 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+fn main() {
+    uniffi::generate_scaffolding("src/http_headermap.udl").unwrap();
+}

fixtures/ext-types/http-headermap/src/http_headermap.udl
@@ -1,0 +1,17 @@
+dictionary HttpHeader {
+    string key;
+    string val;
+};
+
+[Custom]
+typedef sequence<HttpHeader> HeaderMap;
+
+callback interface HeaderMapCallback {
+   HeaderMap run(HeaderMap arg);
+};
+
+namespace ext_types_http_headermap {
+  HeaderMap get_headermap(string value);
+
+  HeaderMap run_callback(HeaderMapCallback arg);
+};

fixtures/ext-types/http-headermap/src/lib.rs
@@ -1,0 +1,57 @@
+use std::str::FromStr;
+
+pub(crate) use http::HeaderMap;
+
+/// A single HttpHeader for Uniffi bindings
+pub struct HttpHeader {
+    pub(crate) key: String,
+    pub(crate) val: String,
+}
+
+/// Expose `http::HeaderMap` to Uniffi.
+impl crate::UniffiCustomTypeConverter for http::HeaderMap {
+    /// http::HeaderMap is a multimap so there may be multiple values
+    /// per key. We represent this as a vector of `HttpHeader` (AKA
+    /// `key` & `val`) where `key` may repeat.
+    type Builtin = Vec<HttpHeader>;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Ok(http::HeaderMap::from_iter(val.into_iter().filter_map(
+            |h| {
+                let n = http::HeaderName::from_str(&h.key).ok()?;
+                let v = http::HeaderValue::from_str(&h.val).ok()?;
+                Some((n, v))
+            },
+        )))
+    }
+
+    fn from_custom(obj: Self) -> Self::Builtin {
+        obj.iter()
+            .map(|(k, v)| HttpHeader {
+                key: k.as_str().to_string(),
+                val: v.to_str().unwrap().to_string(),
+            })
+            .collect()
+    }
+}
+
+pub fn get_headermap(v: String) -> HeaderMap {
+    let n = http::HeaderName::from_str("test-header").unwrap();
+    let v1 = http::HeaderValue::from_str("First value").unwrap();
+    let v2 = http::HeaderValue::from_str(&v).unwrap();
+    HeaderMap::from_iter([(n.clone(), v1), (n, v2)])
+}
+
+pub trait HeaderMapCallback {
+    fn run(&self, arg: HeaderMap) -> HeaderMap;
+}
+
+pub fn run_callback(callback: Box<dyn HeaderMapCallback>) -> HeaderMap {
+    let n = http::HeaderName::from_str("foo").unwrap();
+    let v = http::HeaderValue::from_str("bar").unwrap();
+    let h = HeaderMap::from_iter([(n, v)]);
+
+    callback.run(h)
+}
+
+uniffi::include_scaffolding!("http_headermap");

uniffi_macros/src/custom.rs
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use crate::util::{ident_to_string, mod_path, tagged_impl_header};
-use proc_macro2::{Ident, Span, TokenStream};
+use proc_macro2::{Ident, TokenStream};
 use quote::quote;
 use syn::Path;
 
@@ -11,25 +11,9 @@ use syn::Path;
 // implementation that the library supplies
 pub(crate) fn expand_ffi_converter_custom_type(
     ident: &Ident,
-    builtin: &Ident,
+    builtin: &Path,
     tag: Option<&Path>,
 ) -> syn::Result<TokenStream> {
-    let ffi_name = match builtin.to_string().as_str() {
-        "i8" | "u8" | "i16" | "u16" | "i32" | "u32" | "i64" | "u64" | "f32" | "f64" => {
-            builtin.to_string()
-        }
-        "String" => "::uniffi::RustBuffer".to_string(),
-        // in theory we could support others, but for now...
-        _ => {
-            return Err(syn::Error::new(
-                Span::call_site(),
-                "Type is not supported as a custom type builtin",
-            ));
-        }
-    };
-
-    let ffi_path: TokenStream = ffi_name.parse()?;
-
     let impl_spec = tagged_impl_header("FfiConverter", ident, tag);
     let name = ident_to_string(ident);
     let mod_path = mod_path()?;
@@ -37,7 +21,7 @@ pub(crate) fn expand_ffi_converter_custom_type(
     Ok(quote! {
         #[automatically_derived]
         unsafe #impl_spec {
-            type FfiType = #ffi_path;
+            type FfiType = <#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::FfiType;
             fn lower(obj: #ident ) -> Self::FfiType {
                 <#builtin as ::uniffi::FfiConverter<crate::UniFfiTag>>::lower(<#ident as UniffiCustomTypeConverter>::from_custom(obj))
             }
@@ -67,7 +51,7 @@ pub(crate) fn expand_ffi_converter_custom_type(
 // Generate an FfiConverter impl *and* an UniffiCustomTypeConverter.
 pub(crate) fn expand_ffi_converter_custom_newtype(
     ident: &Ident,
-    builtin: &Ident,
+    builtin: &Path,
     tag: Option<&Path>,
 ) -> syn::Result<TokenStream> {
     let ffi_converter = expand_ffi_converter_custom_type(ident, builtin, tag)?;
@@ -80,7 +64,7 @@ pub(crate) fn expand_ffi_converter_custom_newtype(
     })
 }
 
-fn custom_ffi_type_converter(ident: &Ident, builtin: &Ident) -> syn::Result<TokenStream> {
+fn custom_ffi_type_converter(ident: &Ident, builtin: &Path) -> syn::Result<TokenStream> {
     Ok(quote! {
         impl UniffiCustomTypeConverter for #ident {
             type Builtin = #builtin;

uniffi_macros/src/lib.rs
@@ -14,7 +14,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
     parse::{Parse, ParseStream},
-    parse_macro_input, Ident, LitStr, Token,
+    parse_macro_input, Ident, LitStr, Path, Token,
 };
 
 mod custom;
@@ -44,6 +44,20 @@ impl Parse for IdentPair {
         input.parse::<Token![,]>()?;
         let rhs = input.parse()?;
         Ok(Self { lhs, rhs })
+    }
+}
+
+struct CustomTypeInfo {
+    ident: Ident,
+    builtin: Path,
+}
+
+impl Parse for CustomTypeInfo {
+    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
+        let ident = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let builtin = input.parse()?;
+        Ok(Self { ident, builtin })
     }
 }
 
@@ -135,10 +149,10 @@ pub fn derive_error(input: TokenStream) -> TokenStream {
 /// for a `<T>` which implements `UniffiCustomTypeConverter`.
 #[proc_macro]
 pub fn custom_type(tokens: TokenStream) -> TokenStream {
-    let input: IdentPair = syn::parse_macro_input!(tokens);
+    let input: CustomTypeInfo = syn::parse_macro_input!(tokens);
     custom::expand_ffi_converter_custom_type(
-        &input.lhs,
-        &input.rhs,
+        &input.ident,
+        &input.builtin,
         Some(&syn::parse_quote!(crate::UniFfiTag)),
     )
     .unwrap_or_else(syn::Error::into_compile_error)
@@ -150,10 +164,10 @@ pub fn custom_type(tokens: TokenStream) -> TokenStream {
 /// newtype idiom.
 #[proc_macro]
 pub fn custom_newtype(tokens: TokenStream) -> TokenStream {
-    let input: IdentPair = syn::parse_macro_input!(tokens);
+    let input: CustomTypeInfo = syn::parse_macro_input!(tokens);
     custom::expand_ffi_converter_custom_newtype(
-        &input.lhs,
-        &input.rhs,
+        &input.ident,
+        &input.builtin,
         Some(&syn::parse_quote!(crate::UniFfiTag)),
     )
     .unwrap_or_else(syn::Error::into_compile_error)

fixtures/ext-types/http-headermap/tests/bindings/test_http_headermap.py
@@ -1,0 +1,36 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import unittest
+from ext_types_http_headermap import *
+
+class TestCallback(HeaderMapCallback):
+    def run(self, header_map):
+        self.saw_map = header_map
+        copy = header_map.copy()
+        copy.append(HttpHeader("from-callback", "hello"))
+        return copy
+
+class TestHttpHeaderMap(unittest.TestCase):
+    def test_get_headermap(self):
+        self.assertEqual(get_headermap("Second value"), [
+            HttpHeader("test-header", "First value"),
+            HttpHeader("test-header", "Second value")
+        ])
+
+    def test_guid_callback(self):
+        # Test that we can passing a guid from run_callback() to TestCallback.run() then back out
+
+        test_callback = TestCallback()
+        m = run_callback(test_callback)
+        self.assertEqual(m, [
+            HttpHeader("foo", "bar"),
+            HttpHeader("from-callback", "hello")
+        ])
+        self.assertEqual(test_callback.saw_map, [
+            HttpHeader("foo", "bar"),
+        ])
+
+if __name__=='__main__':
+    unittest.main()

fixtures/ext-types/http-headermap/tests/test_generated_bindings.rs
@@ -1,0 +1,1 @@
+uniffi::build_foreign_language_testcases!("tests/bindings/test_http_headermap.py",);