faa4cbf525165c72d2343fef362377bb00377083
bendk <bendk@users.noreply.github.com>
2023-10-03 17:30:00 +0000

    Built artifacts of 68296e0ba [ci skip]

--- internals/api/uniffi_macros/enum_/sidebar-items.js
+++ internals/api/uniffi_macros/enum_/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["enum_ffi_converter_impl","enum_meta_static_var","enum_or_error_ffi_converter_impl","expand_enum","handle_callback_unexpected_error_fn","rich_error_ffi_converter_impl","variant_metadata","write_field"]};
+window.SIDEBAR_ITEMS = {"fn":["enum_ffi_converter_impl","enum_meta_static_var","enum_or_error_ffi_converter_impl","expand_enum","rich_error_ffi_converter_impl","variant_metadata","write_field"]};
