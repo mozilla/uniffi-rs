10fd058bdd9f7ceda0fd6ae2fdb505f3ed61febf
mhammond <mhammond@users.noreply.github.com>
2023-10-04 14:40:29 +0000

    Built artifacts of b12c7ee14 [ci skip]

--- internals/api/uniffi_macros/export/sidebar-items.js
+++ internals/api/uniffi_macros/export/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["expand_export","ffi_converter_trait_impl","rewrite_self_type"],"mod":["attributes","callback_interface","item","scaffolding"]};
+window.SIDEBAR_ITEMS = {"fn":["expand_export","ffi_converter_trait_impl","rewrite_self_type"],"mod":["attributes","callback_interface","item","scaffolding","utrait"]};
