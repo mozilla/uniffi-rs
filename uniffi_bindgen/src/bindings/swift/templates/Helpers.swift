15a3eb34f5cc41bb770bc982b7de6b91675ccf78
Sebastian Imlay <sebastian.imlay@gmail.com>
2023-11-01 11:28:57 -0400

    Replaced CancellationError with fatalError

--- uniffi_bindgen/src/bindings/swift/templates/Helpers.swift
+++ uniffi_bindgen/src/bindings/swift/templates/Helpers.swift
@@ -93,7 +93,7 @@ private func uniffiCheckCallStatus(
             }
 
         case CALL_CANCELLED:
-                throw CancellationError()
+                throw fatalError("Cancellation not supported yet")
 
         default:
             throw UniffiInternalError.unexpectedRustCallStatusCode
