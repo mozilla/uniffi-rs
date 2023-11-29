a93bdfa249a995c2c53816cd406f497f5ccd0bff
Mark Hammond <mhammond@skippinet.com.au>
2023-11-29 11:48:02 -0500

    Add an arithmetic-procmacro example and tweaks to the examples README (#1857)

---
+++ examples/arithmetic-procmacro/tests/test_generated_bindings.rs
@@ -0,0 +1,3 @@
+// See the tests from the `arithmetic` crate for more comprehensive
+// examples - there's just a Python smoke-test here.
+uniffi::build_foreign_language_testcases!("tests/test.py");
