659401740b02e4b7d856e294dc28a44a67fcce4c
bendk <bendk@users.noreply.github.com>
2023-12-11 13:59:48 +0000

    Built artifacts of 66323a810 [ci skip]

--- internals/api/uniffi/ffi/foreignexecutor/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["ForeignExecutorCallbackResult","RustTaskCallbackCode"],"fn":["foreign_executor_callback_set"],"struct":["ForeignExecutor","ForeignExecutorHandle"],"type":["RustTaskCallback"]};
