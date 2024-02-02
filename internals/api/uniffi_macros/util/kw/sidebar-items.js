39d55dfd01eccc3d2f787ac1fb1d4daefc664552
mhammond <mhammond@users.noreply.github.com>
2024-02-02 14:14:12 +0000

    Built artifacts of b12328a0b [ci skip]

--- internals/api/uniffi_macros/util/kw/sidebar-items.js
+++ internals/api/uniffi_macros/util/kw/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Debug","Display","Eq","Hash","None","async_runtime","callback_interface","constructor","default","flat_error","handle_unknown_callback_error","non_exhaustive","with_foreign","with_try_read"]};
+window.SIDEBAR_ITEMS = {"struct":["Debug","Display","Eq","Hash","None","async_runtime","callback_interface","default","flat_error","handle_unknown_callback_error","non_exhaustive","with_foreign","with_try_read"]};
