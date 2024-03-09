3d698754ea45ff9247279d2594d8186152729a7d
Mark Hammond <mhammond@skippinet.com.au>
2024-03-08 21:35:47 -0500

    Fix Python checking for invalid enum values.
    The generated Python code didn't raise an exception after checking all variants.
    Without the fix the new test fails with:
    
    > enum_types.InternalError: Failed to convert arg 'a': not enough bytes remaining in buffer (0 < 4)
    
    Also upgrades the test to a unittest.

--- fixtures/enum-types/src/lib.rs
+++ fixtures/enum-types/src/lib.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-enum Animal {
+pub enum Animal {
     Dog,
     Cat,
 }
@@ -36,6 +36,11 @@ pub enum AnimalLargeUInt {
 pub enum AnimalSignedInt {
     Dog = -3,
     Cat = -4,
+}
+
+#[uniffi::export]
+fn get_animal(a: Option<Animal>) -> Animal {
+    a.unwrap_or(Animal::Dog)
 }
 
 uniffi::include_scaffolding!("enum_types");
