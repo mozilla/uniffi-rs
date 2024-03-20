d94ee73a13cf9fcf8cae47c0ce61773fcc46cf90
Alexander Cyon <alex.cyon@gmail.com>
2024-03-20 14:33:16 +0100

    Add terse rational for chosen size of BUF_SIZE

--- uniffi_core/src/metadata.rs
+++ uniffi_core/src/metadata.rs
@@ -80,6 +80,9 @@ pub mod codes {
     pub const LIT_NULL: u8 = 4;
 }
 
+// For large errors (e.g. enums) a buffer size of ~4k - ~8k
+// is not enough. See issues on Github: #1968 and #2041 and
+// for an example see fixture/large-error
 const BUF_SIZE: usize = 16384;
 
 // This struct is a kludge around the fact that Rust const generic support doesn't quite handle our
