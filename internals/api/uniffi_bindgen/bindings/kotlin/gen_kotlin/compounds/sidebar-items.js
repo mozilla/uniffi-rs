4e48552b0efb47ade7036dd2408aabdd438890f8
bendk <bendk@users.noreply.github.com>
2024-03-25 14:15:13 +0000

    Built artifacts of 10180bf6f [ci skip]

--- internals/api/uniffi_bindgen/bindings/kotlin/gen_kotlin/compounds/sidebar-items.js
+++ internals/api/uniffi_bindgen/bindings/kotlin/gen_kotlin/compounds/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["render_literal"],"macro":["impl_code_type_for_compound"],"struct":["MapCodeType","OptionalCodeType","SequenceCodeType"]};
+window.SIDEBAR_ITEMS = {"struct":["MapCodeType","OptionalCodeType","SequenceCodeType"]};
--- internals/api/uniffi_macros/util/kw/sidebar-items.js
+++ internals/api/uniffi_macros/util/kw/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Debug","Display","Eq","Hash","None","async_runtime","callback_interface","default","flat_error","handle_unknown_callback_error","name","non_exhaustive","with_foreign","with_try_read"]};
+window.SIDEBAR_ITEMS = {"struct":["Debug","Display","Eq","Hash","None","Some","async_runtime","callback_interface","default","flat_error","handle_unknown_callback_error","name","non_exhaustive","with_foreign","with_try_read"]};
