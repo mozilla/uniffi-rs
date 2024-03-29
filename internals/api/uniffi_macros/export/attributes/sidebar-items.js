33888030ac163c204c1a28452b89afe54d832778
bendk <bendk@users.noreply.github.com>
2024-03-29 13:22:06 +0000

    Built artifacts of 884f7865f [ci skip]

--- internals/api/uniffi_macros/export/attributes/sidebar-items.js
+++ internals/api/uniffi_macros/export/attributes/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AsyncRuntime"],"fn":["ensure_no_path_args"],"struct":["DefaultMap","DefaultPair","ExportFnArgs","ExportImplArgs","ExportStructArgs","ExportTraitArgs","ExportedImplFnArgs","ExportedImplFnAttributes"]};
+window.SIDEBAR_ITEMS = {"enum":["AsyncRuntime"],"fn":["ensure_no_path_args"],"struct":["DefaultMap","DefaultPair","ExportFnArgs","ExportImplArgs","ExportStructArgs","ExportTraitArgs","ExportedImplFnAttributes"]};
