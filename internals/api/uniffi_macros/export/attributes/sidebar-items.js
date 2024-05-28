ce9a4f5b77a7f539e8e11b472ca90a94338ec791
bendk <bendk@users.noreply.github.com>
2024-05-28 14:44:25 +0000

    Built artifacts of e83fc4eae [ci skip]

--- internals/api/uniffi_macros/export/attributes/sidebar-items.js
+++ internals/api/uniffi_macros/export/attributes/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AsyncRuntime"],"fn":["ensure_no_path_args"],"struct":["DefaultMap","DefaultPair","ExportFnArgs","ExportImplArgs","ExportStructArgs","ExportTraitArgs","ExportedImplFnAttributes"]};
+window.SIDEBAR_ITEMS = {"enum":["AsyncRuntime"],"fn":["ensure_no_path_args","is_cfg_attr","is_uniffi_path"],"struct":["DefaultMap","DefaultPair","ExportFnArgs","ExportImplArgs","ExportStructArgs","ExportTraitArgs","ExportedImplFnAttributes"]};
