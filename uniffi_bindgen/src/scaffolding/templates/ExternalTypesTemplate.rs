de382d1b01aa21bbb44e03ff1cd56ac1a6898fdc
Ben Dean-Kawamura <bdk@mozilla.com>
2024-06-11 16:32:16 -0400

    New custom type system
    Implemented a new custom type system described in the new docs.  This
    system is easier to use and also allows us to do some extra tricks
    behind the scene.  For example, I hope to add a flag that will toggle
    between implementing `FfiConverter` for the local tag vs a blanket impl.
    It's not possible to do that with the current system and adding support
    would be awkward.
    
    I wanted to keep backwards compatibility for a bit, but couldn't figure
    out a good way to do it.  The main issue is that for the old system, if
    a custom type is declared in the UDL then we generate the `FfiConverter`
    implementation, while the new system expects the user to call
    `custom_type!` to create the impl.  I tried to get things working by
    creating a blanket impl of `FfiConverter` for types that implemented
    `UniffiCustomTypeConverter`, but ran into issues -- the first blocker I
    found was that there's no way to generate `TYPE_ID_META` since we don't
    know the name of the custom type.
    
    Removed the nested-module-import fixture.  The custom type code will no
    longer test it once we remove the old code, since it's not using
    `UniffiCustomTypeConverter`.  I couldn't think of a way to make it work
    again.

--- uniffi_bindgen/src/scaffolding/templates/ExternalTypesTemplate.rs
+++ uniffi_bindgen/src/scaffolding/templates/ExternalTypesTemplate.rs
@@ -15,8 +15,3 @@
 {%- endmatch %}
 {% endif %}
 {%- endfor %}
-
-// We generate support for each Custom Type and the builtin type it uses.
-{%- for (name, builtin) in ci.iter_custom_types() %}
-::uniffi::custom_type!(r#{{ name }}, {{builtin|type_rs}});
-{%- endfor -%}
