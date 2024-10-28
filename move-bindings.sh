47f71101c510a089a3c3fc8f7458126da42a637a
Mark Hammond <mhammond@skippinet.com.au>
2024-10-27 22:18:48 -0400

    move script

---
+++ move-bindings.sh
@@ -0,0 +1,4 @@
+rm -rf uniffi_bindings/src
+mv uniffi_bindgen/src/bindings uniffi_bindings/src
+mv uniffi_bindings/src/mod.rs uniffi_bindings/src/lib.rs
+mv uniffi_bindgen/src/backend uniffi_bindings/src/backend
