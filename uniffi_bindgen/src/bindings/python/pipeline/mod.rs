782e05f91a9c791bb317b5238553105ebeb89fae
Ben Dean-Kawamura <bdk@mozilla.com>
2025-04-29 15:37:34 -0400

    Python bindings IR -- Copy general IR nodes
    * copied `pipeline/general/nodes.rs` to `python/pipeline/nodes.rs`
    * Removed the `#[wraps]` attributes to avoid wrapping nodes twice.
    * Updated the pipeline to add a general IR -> python IR conversion pass.

--- uniffi_bindgen/src/bindings/python/pipeline/mod.rs
+++ uniffi_bindgen/src/bindings/python/pipeline/mod.rs
@@ -3,9 +3,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 pub use crate::pipeline::{general, initial, Pipeline};
+mod nodes;
+
+pub use nodes::*;
 
 // For now, this is just the general pipeline.
 // Defining this allows us to use the pipeline CLI to inspect the general pipeline.
-pub fn pipeline() -> Pipeline<initial::Root, general::Root> {
-    general::pipeline()
+pub fn pipeline() -> Pipeline<initial::Root, Root> {
+    general::pipeline().convert_ir_pass::<Root>()
 }
