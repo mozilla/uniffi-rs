4625463d9124005269f5663a2902cd217fc5eaac
Mark Hammond <mhammond@skippinet.com.au>
2025-05-29 23:18:20 -0400

    Fix Python mypy errors when record member names match type names.
    Before this, we'd see:
    
    ```
    pub struct Record {
        bool: bool, // sad mypy
    }
    ```
    
    `cargo test -p uniffi-fixture-keywords-rust` works, but:
    
    ```
    % mypy target/tmp/uniffi_keywords_rust-xxx/keywords_rust.py
    .../keywords_rust.py:1161: error: Variable "keywords_rust.RecordWithTypeNames.bool" is not valid as a type  [valid-type]
    ```

--- uniffi_bindgen/src/bindings/python/templates/CallableArgs.py
+++ uniffi_bindgen/src/bindings/python/templates/CallableArgs.py
@@ -2,7 +2,7 @@
     {{ arg.name }}
     {%- match arg.default %}
     {%- when Some(literal) %}: typing.Union[object, {{ arg.ty.type_name }}] = _DEFAULT
-    {%- else %}: {{ arg.ty.type_name }}
+    {%- else %}: {{ arg.ty.type_anno_name }}
     {%- endmatch %}
     {%- if !loop.last %},{% endif -%}
 {%- endfor %}
