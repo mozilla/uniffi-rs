422442381d3a21764eeca0de418655b2914a2d33
Chanjung Kim <chanjungkim@paxbun.dev>
2025-09-06 17:52:40 +0900

    Make CI::is_name_used_as_error consider all known external CIs

--- uniffi_bindgen/src/interface/mod.rs
+++ uniffi_bindgen/src/interface/mod.rs
@@ -1020,7 +1020,9 @@ impl ComponentInterface {
     }
 
     pub fn is_name_used_as_error(&self, name: &str) -> bool {
-        self.errors.contains(name)
+        self.all_component_interfaces
+            .iter()
+            .any(|ci| ci.errors.contains(name))
     }
 
     /// Called by `APIBuilder` impls to add a newly-parsed callback interface definition to the `ComponentInterface`.
