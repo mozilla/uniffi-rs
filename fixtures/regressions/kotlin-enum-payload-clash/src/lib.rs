ece220a60ed7080bbf33ada1505af9bb20be33ca
Praveen Perera <me@praveenperera.com>
2025-10-12 09:42:46 -0500

    Kotlin: Add nested test for fully qualified type names

--- fixtures/regressions/kotlin-enum-payload-clash/src/lib.rs
+++ fixtures/regressions/kotlin-enum-payload-clash/src/lib.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use std::collections::HashMap;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Amount {
     pub value: u64,
@@ -13,9 +15,34 @@ pub enum AmountOrMax {
     Max,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ComplexValue {
+    OptionalAmount {
+        maybe_amount: Option<Amount>,
+    },
+    Amounts {
+        amounts: Vec<Amount>,
+    },
+    AmountMap {
+        amount_map: HashMap<String, Amount>,
+    },
+    NestedOptional {
+        maybe_amounts: Option<Vec<Amount>>,
+    },
+    NestedMap {
+        nested_map: HashMap<String, Vec<Amount>>,
+    },
+}
+
 pub fn get_value() -> AmountOrMax {
     AmountOrMax::Amount {
         amount: Amount { value: 100 },
+    }
+}
+
+pub fn get_complex_value() -> ComplexValue {
+    ComplexValue::Amounts {
+        amounts: vec![Amount { value: 1 }, Amount { value: 2 }],
     }
 }
 
