38493fc0e48effeb3e124e80d1b5759b0f06605c
Jan-Erik Rediger <jrediger@mozilla.com>
2025-10-28 13:24:28 +0100

    Re-do the Docker image, now based on the Swift image
    This is now significantly bigger (7.6 GB), but that's unavoiable with
    the new way Swift is shipped and all the other tooling we need.
    
    Fixes #2892

--- docker/cargo-docker.sh
+++ docker/cargo-docker.sh
@@ -5,9 +5,10 @@
 #  * share cargo registry data with the image, so it doesn't have to re-fetch it each run
 #  * mount the current working directory into the image, and run in that directory
 #  * run via bash in interactive mode to get correct $PATH setup from .bashrc
-docker run \
+exec docker run \
     -ti --rm \
     -v $HOME/.cargo/registry:/usr/local/cargo/registry \
     -v $PWD:/mounted_workdir \
     -w /mounted_workdir \
-    rfkelly/uniffi-ci:latest bash -i -c "cargo $*"
+    --platform linux/amd64 \
+    janerik/uniffi-ci-test:latest bash -i -c "cargo $*"
