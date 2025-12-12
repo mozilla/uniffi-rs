6e43992a727c1571711debf6ead8a811fdd8baa5
Ben Dean-Kawamura <bdk@mozilla.com>
2025-12-12 12:03:33 -0500

    Kotlin: Use pointer FFI for RustBuffer functions
    The new versions of this functions don't use a `RustCallStatus` since
    the operations are infallible (failure to allocate results in a panic).
    
    I considered making `rust_buffer_alloc` input an integer instead of a
    pointer.  It feels a bit silly creating a buffer just to pick a single
    int into it.  However, issues like #2740 make me want to avoid ints, for
    now at least.  Once we've established some baseline stability, then we
    can investigate changing the signatures.

--- uniffi_bindgen/src/interface/pointer_ffi.rs
+++ uniffi_bindgen/src/interface/pointer_ffi.rs
@@ -20,64 +20,72 @@ pub struct PointerFfiFunction {
 }
 
 pub fn ffi_definitions(ci: &ComponentInterface) -> impl Iterator<Item = PointerFfiDefinition> + '_ {
-    [].into_iter()
-        // Functions
-        .chain(
-            ci.function_definitions()
-                .iter()
-                .map(|f| PointerFfiDefinition::func(f.ffi_func().pointer_ffi_name())),
-        )
-        // Constructors
-        .chain(ci.object_definitions().iter().flat_map(|o| {
-            o.constructors()
-                .into_iter()
-                .map(|c| PointerFfiDefinition::func(c.ffi_func().pointer_ffi_name()))
-        }))
-        // Methods
-        .chain(
-            ci.enum_definitions()
-                .iter()
-                .flat_map(|e| e.methods())
-                .chain(
-                    ci.record_definitions()
-                        .iter()
-                        .flat_map(|r| r.methods()),
-                )
-                .chain(
-                    ci.object_definitions()
-                        .iter()
-                        .flat_map(|o| o.methods()),
-                )
-                .map(|m| PointerFfiDefinition::func(m.ffi_func().pointer_ffi_name())),
-        )
-        // UniFFI trait methods
-        .chain(
-            ci.enum_definitions()
-                .iter()
-                .flat_map(|e| e.uniffi_traits())
-                .chain(
-                    ci.record_definitions()
-                        .iter()
-                        .flat_map(|r| r.uniffi_traits()),
-                )
-                .chain(
-                    ci.object_definitions()
-                        .iter()
-                        .flat_map(|o| o.uniffi_traits()),
-                )
-                .flat_map(|ut| match ut {
-                    UniffiTrait::Display { fmt: m }
-                    | UniffiTrait::Debug { fmt: m }
-                    | UniffiTrait::Hash { hash: m }
-                    | UniffiTrait::Ord { cmp: m } => vec![m],
-                    UniffiTrait::Eq { eq, ne } => vec![eq, ne],
-                })
-                .map(|m| PointerFfiDefinition::func(m.ffi_func().pointer_ffi_name())),
-        )
+    let namespace = ci.ffi_namespace();
+    // Builtin FFI methods
+    [
+        PointerFfiDefinition::func(format!("uniffi_ptr_{namespace}_rustbuffer_alloc")),
+        PointerFfiDefinition::func(format!("uniffi_ptr_{namespace}_rustbuffer_free")),
+    ]
+    .into_iter()
+    // Functions
+    .chain(
+        ci.function_definitions()
+            .iter()
+            .map(|f| PointerFfiDefinition::func(f.ffi_func().pointer_ffi_name())),
+    )
+    // Constructors
+    .chain(ci.object_definitions().iter().flat_map(|o| {
+        o.constructors()
+            .into_iter()
+            .map(|c| PointerFfiDefinition::func(c.ffi_func().pointer_ffi_name()))
+    }))
+    // Methods
+    .chain(
+        ci.enum_definitions()
+            .iter()
+            .flat_map(|e| e.methods())
+            .chain(ci.record_definitions().iter().flat_map(|r| r.methods()))
+            .chain(ci.object_definitions().iter().flat_map(|o| o.methods()))
+            .map(|m| PointerFfiDefinition::func(m.ffi_func().pointer_ffi_name())),
+    )
+    // UniFFI trait methods
+    .chain(
+        ci.enum_definitions()
+            .iter()
+            .flat_map(|e| e.uniffi_traits())
+            .chain(
+                ci.record_definitions()
+                    .iter()
+                    .flat_map(|r| r.uniffi_traits()),
+            )
+            .chain(
+                ci.object_definitions()
+                    .iter()
+                    .flat_map(|o| o.uniffi_traits()),
+            )
+            .flat_map(|ut| match ut {
+                UniffiTrait::Display { fmt: m }
+                | UniffiTrait::Debug { fmt: m }
+                | UniffiTrait::Hash { hash: m }
+                | UniffiTrait::Ord { cmp: m } => vec![m],
+                UniffiTrait::Eq { eq, ne } => vec![eq, ne],
+            })
+            .map(|m| PointerFfiDefinition::func(m.ffi_func().pointer_ffi_name())),
+    )
 }
 
 impl PointerFfiDefinition {
     fn func(name: impl Into<String>) -> Self {
         Self::Function(PointerFfiFunction { name: name.into() })
+    }
+}
+
+impl ComponentInterface {
+    pub fn pointer_ffi_rustbuffer_alloc(&self) -> String {
+        format!("uniffi_ptr_{}_rustbuffer_alloc", self.ffi_namespace())
+    }
+
+    pub fn pointer_ffi_rustbuffer_free(&self) -> String {
+        format!("uniffi_ptr_{}_rustbuffer_free", self.ffi_namespace())
     }
 }
