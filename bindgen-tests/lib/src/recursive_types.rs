fc3794f42f001c2728bf80ec55ae9b7552298c14
Ben Dean-Kawamura <bdk@mozilla.com>
2026-03-20 10:31:38 -0400

    Move recursive type tests to bindings-tests
    We just added these tests and @mhammond pointed out that we should have
    added them to `bindgen-tests` rather than creating a new fixture.  Moved
    the files over and added a new `bindgen-tests` feature for it.
    
    Reordered the feature list so they're in the order you would likely want to
    implement them for a new bindings generator.

--- bindgen-tests/lib/src/recursive_types.rs
+++ bindgen-tests/lib/src/recursive_types.rs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-uniffi::setup_scaffolding!("recursive_enums");
-
 // Self-recursive tree
 #[derive(uniffi::Enum)]
 pub enum Tree {
--- bindgen-tests/swift/tests/recursive_types.swift
+++ bindgen-tests/swift/tests/recursive_types.swift
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import recursive_enums
+import uniffi_bindgen_tests
 
 // Tree (self-recursive)
 let leaf = Tree.leaf(5)
