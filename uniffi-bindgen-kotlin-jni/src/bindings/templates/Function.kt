b4f4d02906be56d92bf90ea5643b814ec33a827f
Ben Dean-Kawamura <bdk@mozilla.com>
2026-03-29 10:13:28 -0400

    uniffi-bindgen-kotlin-jni: primitive types
    Pass ints, floats and strings using JNI. Added a section in `DESIGN.md`
    and some code in `ffi_types.rs` describing the plan to pass other types
    as primitives.
    
    Added the `ArgFfi` and `ReturnFfi` enums to prepare for other methods of
    passing values across the FFI.  Right now there's just one variant, but
    I'll be adding another soon.

--- uniffi-bindgen-kotlin-jni/src/bindings/templates/Function.kt
+++ uniffi-bindgen-kotlin-jni/src/bindings/templates/Function.kt
@@ -1,3 +1,5 @@
-public fun {{ func.callable.name_kt() }}() {
-    uniffi.Scaffolding.{{ func.jni_method_name }}()
+public fun {{ func.callable.name_kt() }}({{ func.callable.arg_list_kt() }}): {{ func.callable.return_type_kt() }} {
+    {%- let callable = func.callable %}
+    {%- let jni_method_name = func.jni_method_name %}
+    {%- filter indent(4) %}{% include "CallableBody.kt" %}{% endfilter %}
 }
