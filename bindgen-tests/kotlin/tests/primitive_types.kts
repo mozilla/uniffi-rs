990f4f8dc55df0fa7194ce68db6c616e6f9bdce6
Ben Dean-Kawamura <bdk@mozilla.com>
2026-04-01 09:32:40 -0400

    More primitive types tests
    Added tests for input and output only.  This allows you to test one half
    of the FFI at a time and implement things incrementally.  Also, I want
    to implement the new Kotlin FFI slightly differently depending on if you
    need to input an argument or not and these new tests cover the cases
    better.

--- bindgen-tests/kotlin/tests/primitive_types.kts
+++ bindgen-tests/kotlin/tests/primitive_types.kts
@@ -1,5 +1,32 @@
 import uniffi.uniffi_bindgen_tests.*
 
+// Test passing arguments from Kotlin to Rust
+inputU8(42.toUByte())
+inputI8((-42).toByte())
+inputU16(42.toUShort())
+inputI16((-42).toShort())
+inputU32(42u)
+inputI32(-42)
+inputU64(42uL)
+inputI64(-42L)
+inputF32(0.5f)
+inputF64(-3.5)
+inputBool(true)
+inputString("ABC")
+// Test returning values from Rust to Kotlin
+assert(outputU8() == 1.toUByte())
+assert(outputI8() == 1.toByte())
+assert(outputU16() == 1.toUShort())
+assert(outputI16() == 1.toShort())
+assert(outputU32() == 1u)
+assert(outputI32() == 1)
+assert(outputU64() == 1uL)
+assert(outputI64() == 1L)
+assert(outputF32() == 1.0f)
+assert(outputF64() == 1.0)
+assert(outputBool() == true)
+assert(outputString() == "test-string")
+// Test passing values both ways
 assert(roundtripU8(42.toUByte()) == 42.toUByte())
 assert(roundtripI8((-42).toByte()) == (-42).toByte())
 assert(roundtripU16(42.toUShort()) == 42.toUShort())
--- bindgen-tests/python/tests/primitive_types.py
+++ bindgen-tests/python/tests/primitive_types.py
@@ -2,7 +2,35 @@ from test_package.uniffi_bindgen_tests import *
 import unittest
 
 class TestPrimitiveTypes(unittest.TestCase):
-    def test_primitives(self):
+    def test_input(self):
+        input_u8(42)
+        input_i8(-42)
+        input_u16(42)
+        input_i16(-42)
+        input_u32(42)
+        input_i32(-42)
+        input_u64(42)
+        input_i64(-42)
+        input_f32(0.5)
+        input_f64(-3.5)
+        input_bool(True)
+        input_string("ABC")
+
+    def test_output(self):
+        self.assertEqual(output_u8(), 1)
+        self.assertEqual(output_i8(), 1)
+        self.assertEqual(output_u16(), 1)
+        self.assertEqual(output_i16(), 1)
+        self.assertEqual(output_u32(), 1)
+        self.assertEqual(output_i32(), 1)
+        self.assertEqual(output_u64(), 1)
+        self.assertEqual(output_i64(), 1)
+        self.assertEqual(output_f32(), 1.0)
+        self.assertEqual(output_f64(), 1.0)
+        self.assertEqual(output_bool(), True)
+        self.assertEqual(output_string(), "test-string")
+
+    def test_roundtrip(self):
         self.assertEqual(roundtrip_u8(42), 42)
         self.assertEqual(roundtrip_i8(-42), -42)
         self.assertEqual(roundtrip_u16(42), 42)
