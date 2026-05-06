78cfd1ed12bdd01140b0d6ba9ab787b499fe9298
Ben Dean-Kawamura <bdk@mozilla.com>
2026-05-06 19:18:15 -0400

    Adding more optional/vec tests
    I'm planning on special casing these in for uniffi-bindgen-kotlin-jni,
    and it seems likely that other languages will want to do the same.
    Let's make sure we're testing as many different cases here.
    
    Removed the `strings_with_bom` test from `primitive_types.rs`.  I think
    it's better if that one doesn't include a Vec, so that languages can
    run the test before they have vec support.  I considered moving this to
    the `compound_types` test, but I don't think there's anything particular
    tricky about a vec of strings vs a single string so I just deleted it.

--- bindgen-tests/kotlin/tests/compound_types.kts
+++ bindgen-tests/kotlin/tests/compound_types.kts
@@ -1,14 +1,73 @@
 import uniffi.uniffi_bindgen_tests.*
 
-assert(roundtripOption(42u)!! == 42u)
-assert(roundtripOption(null) == null)
-assert(roundtripVec(listOf(1u, 2u, 3u)) == listOf(1u, 2u, 3u))
+assert(roundtripOptionU8(67.toUByte()) == 67.toUByte());
+assert(roundtripOptionU8(null) == null);
+assert(roundtripOptionI8(67.toByte()) == 67.toByte());
+assert(roundtripOptionI8(null) == null);
+assert(roundtripOptionU16(67.toUShort()) == 67.toUShort());
+assert(roundtripOptionU16(null) == null);
+assert(roundtripOptionI16(67.toShort()) == 67.toShort());
+assert(roundtripOptionI16(null) == null);
+assert(roundtripOptionU32(67u) == 67u);
+assert(roundtripOptionU32(null) == null);
+assert(roundtripOptionI32(67) == 67);
+assert(roundtripOptionI32(null) == null);
+assert(roundtripOptionU64(67uL) == 67uL);
+assert(roundtripOptionU64(null) == null);
+assert(roundtripOptionI64(67L) == 67L);
+assert(roundtripOptionI64(null) == null);
+assert(roundtripOptionF32(67.0f) == 67.0f);
+assert(roundtripOptionF32(null) == null);
+assert(roundtripOptionF64(67.0) == 67.0);
+assert(roundtripOptionF64(null) == null);
+assert(roundtripOptionBool(true) == true);
+assert(roundtripOptionBool(null) == null);
+assert(roundtripOptionString("test-string") == "test-string");
+assert(roundtripOptionString(null) == null);
+assert(roundtripOptionRec(CompoundTypesRec(67.toUByte())) == CompoundTypesRec(67.toUByte()))
+assert(roundtripOptionRec(null) == null);
+
+assert(roundtripVecI8(listOf(1.toByte(), 2.toByte(), 3.toByte())) == listOf(1.toByte(), 2.toByte(), 3.toByte()));
+assert(roundtripVecU16(listOf(1.toUShort(), 2.toUShort(), 3.toUShort())) == listOf(1.toUShort(), 2.toUShort(), 3.toUShort()));
+assert(roundtripVecI16(listOf(1.toShort(), 2.toShort(), 3.toShort())) == listOf(1.toShort(), 2.toShort(), 3.toShort()));
+assert(roundtripVecU32(listOf(1u, 2u, 3u)) == listOf(1u, 2u, 3u));
+assert(roundtripVecI32(listOf(1, 2, 3)) == listOf(1, 2, 3));
+assert(roundtripVecU64(listOf(1uL, 2uL, 3uL)) == listOf(1uL, 2uL, 3uL));
+assert(roundtripVecI64(listOf(1L, 2L, 3L)) == listOf(1L, 2L, 3L));
+assert(roundtripVecF32(listOf(1.0f, 2.0f, 3.0f)) == listOf(1.0f, 2.0f, 3.0f));
+assert(roundtripVecF64(listOf(1.0, 2.0, 3.0)) == listOf(1.0, 2.0, 3.0));
+assert(roundtripVecBool(listOf(true, false)) == listOf(true, false));
+assert(roundtripVecString(listOf("test-string")) == listOf("test-string"));
+assert(roundtripVecRec(listOf(CompoundTypesRec(67.toUByte()))) == listOf(CompoundTypesRec(67.toUByte())))
+
 assert(roundtripHashMap(mapOf("a" to 1u, "b" to 2u)) == mapOf("a" to 1u, "b" to 2u))
 assert(roundtripHashSet(setOf("a", "b", "c")) == setOf("a", "b", "c"))
+assert(roundtripHashMapU32Key(mapOf(1u to 2u, 2u to 4u)) == mapOf(1u to 2u, 2u to 4u))
+
+assert(
+    RecWithCompounds(
+        EnumWithCompounds.A(null),
+        null,
+        listOf(true, false),
+        mapOf("a" to 10.toUByte(), "b" to 20.toUByte()),
+    ) == RecWithCompounds(
+        EnumWithCompounds.A(null),
+        null,
+        listOf(true, false),
+        mapOf("a" to 10.toUByte(), "b" to 20.toUByte()),
+    )
+)
+
 assert(roundtripComplexCompound(listOf(
-    mapOf("a" to 1u, "b" to 2u)
+    mapOf(
+        "a" to CompoundTypesComplexRec(10u, "Test", CompoundTypesEnum.A(100)),
+        "b" to CompoundTypesComplexRec(20u, "Test2", CompoundTypesEnum.B(1.0f, true))
+    )
 )) == listOf(
-    mapOf("a" to 1u, "b" to 2u)
+    mapOf(
+        "a" to CompoundTypesComplexRec(10u, "Test", CompoundTypesEnum.A(100)),
+        "b" to CompoundTypesComplexRec(20u, "Test2", CompoundTypesEnum.B(1.0f, true))
+    )
 ))
 assert(roundtripComplexCompound(null) == null)
 assert(roundtripComplexHashSet(listOf(setOf("a", "b"))) == listOf(setOf("a", "b")))
--- bindgen-tests/swift/tests/compound_types.swift
+++ bindgen-tests/swift/tests/compound_types.swift
@@ -4,15 +4,79 @@
 
 import uniffi_bindgen_tests
 
-assert(roundtripOption(a: 42) == 42);
-assert(roundtripOption(a: nil) == nil);
-assert(roundtripVec(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripOptionU8(a: 67) == 67);
+assert(roundtripOptionU8(a: nil) == nil);
+assert(roundtripOptionI8(a: 67) == 67);
+assert(roundtripOptionI8(a: nil) == nil);
+assert(roundtripOptionU16(a: 67) == 67);
+assert(roundtripOptionU16(a: nil) == nil);
+assert(roundtripOptionI16(a: 67) == 67);
+assert(roundtripOptionI16(a: nil) == nil);
+assert(roundtripOptionU32(a: 67) == 67);
+assert(roundtripOptionU32(a: nil) == nil);
+assert(roundtripOptionI32(a: 67) == 67);
+assert(roundtripOptionI32(a: nil) == nil);
+assert(roundtripOptionU64(a: 67) == 67);
+assert(roundtripOptionU64(a: nil) == nil);
+assert(roundtripOptionI64(a: 67) == 67);
+assert(roundtripOptionI64(a: nil) == nil);
+assert(roundtripOptionF32(a: 67.0) == 67.0);
+assert(roundtripOptionF32(a: nil) == nil);
+assert(roundtripOptionF64(a: 67.0) == 67.0);
+assert(roundtripOptionF64(a: nil) == nil);
+assert(roundtripOptionBool(a: true) == true);
+assert(roundtripOptionBool(a: nil) == nil);
+assert(roundtripOptionString(a: "test-string") == "test-string");
+assert(roundtripOptionString(a: nil) == nil);
+assert(roundtripOptionRec(a: CompoundTypesRec(a: 67)) == CompoundTypesRec(a: 67))
+assert(roundtripOptionRec(a: nil) == nil);
+
+assert(roundtripVecI8(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecU16(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecI16(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecU32(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecI32(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecU64(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecI64(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecF32(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecF64(a: [1, 2, 3]) == [1, 2, 3]);
+assert(roundtripVecBool(a: [true, false]) == [true, false]);
+assert(roundtripVecString(a: ["test-string"]) == ["test-string"]);
+assert(roundtripVecRec(a: [CompoundTypesRec(a: 67)]) == [CompoundTypesRec(a: 67)])
+
 assert(roundtripHashMap(a: ["a": 1, "b": 2]) == ["a": 1, "b": 2])
 assert(roundtripHashSet(a: ["a", "b", "c"]) == ["a", "b", "c"])
+assert(roundtripHashMapU32Key(a: [1: 2, 2: 4]) == [1: 2, 2: 4])
+
+assert(
+    RecWithCompounds(
+        a: EnumWithCompounds.a(nil),
+        b: nil,
+        c: [true, false],
+        d: [
+            "a": 10,
+            "b": 20,
+        ]
+    ) == RecWithCompounds(
+        a: EnumWithCompounds.a(nil),
+        b: nil,
+        c: [true, false],
+        d: [
+            "a": 10,
+            "b": 20,
+        ]
+    )
+)
 assert(roundtripComplexCompound(a: [
-    ["a": 1, "b": 2]
+    [
+        "a": CompoundTypesComplexRec(a: 10, b: "Test", c: CompoundTypesEnum.a(100)),
+        "b": CompoundTypesComplexRec(a: 20, b: "Test2", c: CompoundTypesEnum.b(a: 1.0, b: true))
+    ]
 ]) == [
-    ["a": 1, "b": 2]
+    [
+        "a": CompoundTypesComplexRec(a: 10, b: "Test", c: CompoundTypesEnum.a(100)),
+        "b": CompoundTypesComplexRec(a: 20, b: "Test2", c: CompoundTypesEnum.b(a: 1.0, b: true))
+    ]
 ])
 assert(roundtripComplexCompound(a: nil) == nil);
 assert(roundtripComplexHashSet(a: [["a", "b"]]) == [["a", "b"]])
