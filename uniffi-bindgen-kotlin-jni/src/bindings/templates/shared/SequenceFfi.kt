853a95733a36171d47689f81140815015b07dc6d
Ben Dean-Kawamura <bdk@mozilla.com>
2026-05-26 10:47:39 -0400

    uniffi-bindgen-kotlin-jni: primitive vecs
    Pass these as JVM arrays which gives a nice performance boost.

--- uniffi-bindgen-kotlin-jni/src/bindings/templates/shared/SequenceFfi.kt
+++ uniffi-bindgen-kotlin-jni/src/bindings/templates/shared/SequenceFfi.kt
@@ -1,7 +1,8 @@
 {%- let type_name = seq.self_type.type_kt %}
+{%- let lift_from_buffer = "liftFromBuffer{}"|format(seq.self_type.id) %}
+{%- let lower_to_buffer = "lowerToBuffer{}"|format(seq.self_type.id) %}
 
-@JvmName("{{ seq.self_type.lower_fn_kt() }}")
-fun {{ seq.self_type.lower_fn_kt() }}(value: {{ type_name }}): java.nio.ByteBuffer {
+fun {{ lower_to_buffer }}(value: {{ type_name }}): java.nio.ByteBuffer {
     val buf = Scaffolding.ffiBufferAlloc(value.size * {{ seq.item_size }}).order(java.nio.ByteOrder.nativeOrder())
     var offset = 0
     value.forEach {
@@ -11,8 +12,7 @@ fun {{ seq.self_type.lower_fn_kt() }}(value: {{ type_name }}): java.nio.ByteBuff
     return buf
 }
 
-@JvmName("{{ seq.self_type.lift_fn_kt() }}")
-fun {{ seq.self_type.lift_fn_kt() }}(buf: java.nio.ByteBuffer?): {{ type_name }} {
+fun {{ lift_from_buffer }}(buf: java.nio.ByteBuffer?): {{ type_name }} {
     val buf = buf!!.order(java.nio.ByteOrder.nativeOrder())
     try {
         val len = buf.capacity() / {{ seq.item_size }}
@@ -28,12 +28,26 @@ fun {{ seq.self_type.lift_fn_kt() }}(buf: java.nio.ByteBuffer?): {{ type_name }}
     }
 }
 
+{%- if !seq.is_primitive_array %}
+
+@JvmName("{{ seq.self_type.lower_fn_kt() }}")
+fun {{ seq.self_type.lower_fn_kt() }}(value: {{ type_name }}): java.nio.ByteBuffer {
+    return {{ lower_to_buffer }}(value)
+}
+
+@JvmName("{{ seq.self_type.lift_fn_kt() }}")
+fun {{ seq.self_type.lift_fn_kt() }}(buf: java.nio.ByteBuffer?): {{ type_name }} {
+    return {{ lift_from_buffer }}(buf)
+}
+
+{%- endif %}
+
 fun {{ seq.self_type.write_fn_kt() }}(buf: java.nio.ByteBuffer, offset: kotlin.Int, value: {{ type_name }}) {
-    val childBuf = {{ seq.self_type.lower_fn_kt() }}(value)
+    val childBuf = {{ lower_to_buffer }}(value)
     writeBuffer(buf, offset, childBuf)
 }
 
 fun {{ seq.self_type.read_fn_kt() }}(buf: java.nio.ByteBuffer, offset: kotlin.Int): {{ type_name }} {
     val childBuf = readBuffer(buf, offset)
-    return {{ seq.self_type.lift_fn_kt() }}(childBuf)
+    return {{ lift_from_buffer }}(childBuf)
 }
