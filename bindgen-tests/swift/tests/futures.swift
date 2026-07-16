a6f7efaeab7d2148b92368635936ceefc0e96184
Ben Dean-Kawamura <bdk@mozilla.com>
2026-07-16 13:53:08 -0400

    Swift concurrency fixes for bindgen-tests
    Add `nonisolated(unsafe)` to global variables and put them behind a lock
    just in case there really was a concurrency issue with how we were
    accessing them.
    
    Don't use DispatchGroups/Tasks to schedule the async code anymore,
    instead just call the async functions directly. The old code hangs Swift
    6 It's not completely clear to me, but I think it has to do with the
    fact that the way we were creating tasks was not compatible with
    whatever async context Swift creates for the script.
    
    This makes the tests pass for me in both swift 5 and swift 6 mode.

--- bindgen-tests/swift/tests/futures.swift
+++ bindgen-tests/swift/tests/futures.swift
@@ -5,100 +5,75 @@
 import Foundation
 import uniffi_bindgen_tests
 
-var dispatchGroup = DispatchGroup()
-
 // Primitive types
-dispatchGroup.enter()
-Task {
-    let returnedU8 = await asyncRoundtripU8(v: 42)
-    assert(returnedU8 == 42)
+let returnedU8 = await asyncRoundtripU8(v: 42)
+assert(returnedU8 == 42)
 
-    let returnedI8 = await asyncRoundtripI8(v: -42)
-    assert(returnedI8 == -42)
+let returnedI8 = await asyncRoundtripI8(v: -42)
+assert(returnedI8 == -42)
 
-    let returnedU16 = await asyncRoundtripU16(v: 42)
-    assert(returnedU16 == 42)
+let returnedU16 = await asyncRoundtripU16(v: 42)
+assert(returnedU16 == 42)
 
-    let returnedI16 = await asyncRoundtripI16(v: -42)
-    assert(returnedI16 == -42)
+let returnedI16 = await asyncRoundtripI16(v: -42)
+assert(returnedI16 == -42)
 
-    let returnedU32 = await asyncRoundtripU32(v: 42)
-    assert(returnedU32 == 42)
+let returnedU32 = await asyncRoundtripU32(v: 42)
+assert(returnedU32 == 42)
 
-    let returnedI32 = await asyncRoundtripI32(v: -42)
-    assert(returnedI32 == -42)
+let returnedI32 = await asyncRoundtripI32(v: -42)
+assert(returnedI32 == -42)
 
-    let returnedU64 = await asyncRoundtripU64(v: 42)
-    assert(returnedU64 == 42)
+let returnedU64 = await asyncRoundtripU64(v: 42)
+assert(returnedU64 == 42)
 
-    let returnedI64 = await asyncRoundtripI64(v: -42)
-    assert(returnedI64 == -42)
+let returnedI64 = await asyncRoundtripI64(v: -42)
+assert(returnedI64 == -42)
 
-    let returnedF32 = await asyncRoundtripF32(v: 0.5)
-    assert(returnedF32 == 0.5)
+let returnedF32 = await asyncRoundtripF32(v: 0.5)
+assert(returnedF32 == 0.5)
 
-    let returnedF64 = await asyncRoundtripF64(v: -0.5)
-    assert(returnedF64 == -0.5)
+let returnedF64 = await asyncRoundtripF64(v: -0.5)
+assert(returnedF64 == -0.5)
 
-    let returnedString = await asyncRoundtripString(v: "hi")
-    assert(returnedString == "hi")
+let returnedString = await asyncRoundtripString(v: "hi")
+assert(returnedString == "hi")
 
-    let returnedList = await asyncRoundtripVec(v: [42])
-    assert(returnedList == [42])
+let returnedList = await asyncRoundtripVec(v: [42])
+assert(returnedList == [42])
 
-    let returnedMap = await asyncRoundtripMap(v: ["hello": "world"])
-    assert(returnedMap == ["hello": "world"])
-
-    dispatchGroup.leave()
-}
-dispatchGroup.wait()
+let returnedMap = await asyncRoundtripMap(v: ["hello": "world"])
+assert(returnedMap == ["hello": "world"])
 
 // Errors
-dispatchGroup.enter()
-Task {
-    do {
-        try await asyncThrowError()
-        fatalError("expected asyncThrowError to throw")
-    } catch TestError.Failure1 {
-        // Expected
-    } catch {
-        fatalError("unexpected error \(error)")
-    }
-  dispatchGroup.leave()
+do {
+    try await asyncThrowError()
+    fatalError("expected asyncThrowError to throw")
+} catch TestError.Failure1 {
+    // Expected
+} catch {
+    fatalError("unexpected error \(error)")
 }
-dispatchGroup.wait()
 
 // Interfaces/methods
-dispatchGroup.enter()
-Task {
-    let obj = AsyncInterface(name: "Alice")
-    let objName = await obj.name()
-    assert(objName == "Alice")
+let obj = AsyncInterface(name: "Alice")
+let objName = await obj.name()
+assert(objName == "Alice")
 
-    let obj2 = await asyncRoundtripObj(v: obj)
-    let obj2Name = await obj2.name()
-    assert(obj2Name == "Alice")
+let obj2 = await asyncRoundtripObj(v: obj)
+let obj2Name = await obj2.name()
+assert(obj2Name == "Alice")
 
-    let rec = AsyncRecord(name: "Bob")
-    let recName = await rec.getName()
-    assert(recName == "Bob")
-
-    dispatchGroup.leave()
-}
-dispatchGroup.wait()
+let rec = AsyncRecord(name: "Bob")
+let recName = await rec.getName()
+assert(recName == "Bob")
 
 // Callback interfaces
-var asyncCallbackRefCount = 0
 class AsyncCallbackImpl: TestAsyncCallbackInterface, @unchecked Sendable {
     var value: UInt32;
 
     init(value: UInt32) {
         self.value = value;
-        asyncCallbackRefCount += 1
-    }
-
-    deinit {
-        asyncCallbackRefCount -= 1
     }
 
     func noop() async { }
@@ -119,36 +94,29 @@ class AsyncCallbackImpl: TestAsyncCallbackInterface, @unchecked Sendable {
     }
 }
 
-dispatchGroup.enter()
-Task {
-    let cbi = AsyncCallbackImpl(value: 42);
-    await invokeTestAsyncCallbackInterfaceNoop(cbi: cbi);
+let cbi = AsyncCallbackImpl(value: 42);
+await invokeTestAsyncCallbackInterfaceNoop(cbi: cbi);
 
-    var value = await invokeTestAsyncCallbackInterfaceGetValue(cbi: cbi)
-    assert(value == 42)
+var value = await invokeTestAsyncCallbackInterfaceGetValue(cbi: cbi)
+assert(value == 42)
 
-    await invokeTestAsyncCallbackInterfaceSetValue(cbi: cbi, value: 43);
-    value = await invokeTestAsyncCallbackInterfaceGetValue(cbi: cbi)
-    assert(value == 43)
+await invokeTestAsyncCallbackInterfaceSetValue(cbi: cbi, value: 43);
+value = await invokeTestAsyncCallbackInterfaceGetValue(cbi: cbi)
+assert(value == 43)
 
-    do {
-        let _ = try await invokeTestAsyncCallbackInterfaceThrowIfEqual(
-            cbi: cbi,
-            numbers: CallbackInterfaceNumbers(a: 10, b: 10)
-        )
-    } catch TestError.Failure1 {
-        // expected
-    } catch {
-        fatalError("unexpected error \(error)")
-    } 
-
-    let returnedNumbers = try! await invokeTestAsyncCallbackInterfaceThrowIfEqual(
+do {
+    let _ = try await invokeTestAsyncCallbackInterfaceThrowIfEqual(
         cbi: cbi,
-        numbers: CallbackInterfaceNumbers(a: 10, b: 11)
+        numbers: CallbackInterfaceNumbers(a: 10, b: 10)
     )
-    assert(returnedNumbers == CallbackInterfaceNumbers(a: 10, b: 11))
+} catch TestError.Failure1 {
+    // expected
+} catch {
+    fatalError("unexpected error \(error)")
+} 
 
-    dispatchGroup.leave()
-}
-dispatchGroup.wait()
-
+let returnedNumbers = try! await invokeTestAsyncCallbackInterfaceThrowIfEqual(
+    cbi: cbi,
+    numbers: CallbackInterfaceNumbers(a: 10, b: 11)
+)
+assert(returnedNumbers == CallbackInterfaceNumbers(a: 10, b: 11))
