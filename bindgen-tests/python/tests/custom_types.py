67134079c393e632dec83f7a6eb3ab79a941ac6a
Ben Dean-Kawamura <bdk@mozilla.com>
2026-07-20 11:21:26 -0400

    Python: add custom types to __all__ (#2852)

--- bindgen-tests/python/tests/custom_types.py
+++ bindgen-tests/python/tests/custom_types.py
@@ -3,11 +3,15 @@ import unittest
 
 class TestCustomTypes(unittest.TestCase):
     def test_custom_type(self):
-        assert(roundtrip_custom_type1(100) == 100)
+        self.assertEqual(roundtrip_custom_type1(100), 100)
 
     def test_custom_type_with_config(self):
-        assert(roundtrip_custom_type2({ "value": 200 }) == { "value": 200 })
+        self.assertEqual(roundtrip_custom_type2({ "value": 200 }), { "value": 200 })
 
+
+    def test_types(self):
+        self.assertEqual(CustomType1, int)
+        self.assertEqual(CustomType2, dict[str, str])
 
 if __name__ == '__main__':
     unittest.main()
